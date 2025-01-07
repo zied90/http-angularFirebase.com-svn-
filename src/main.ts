00768c78ba4d8a795b3a603cd9e51fb1b87a8d6e
zied <zied.elmiladi@gmail.com>
2025-01-07 15:12:01 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,158 +1,117 @@
-Fast refresh only works when a file only exports components. Use a new file to share constants or functions between components.eslint(react-refresh/only-export-components)
-const createProtectedRouter: (userRole: UserRole) => Router
 
+ DEV  v2.1.2 C:/dev/Front/consolepfel-refonte-front
 
-import { Layout } from "@/components/layout/Layout";
-import DocumentsList from "@/pages/esign-demat/document";
-import FormSetting from "@/pages/esign-demat/document/FormSetting";
-import { GroupeList } from "@/pages/esign-demat/groupe";
-import LettreList from "@/pages/esign-demat/lettre";
-import PerimetreMetierList from "@/pages/esign-demat/perimetreMetier";
-import PrePrintedList from "@/pages/esign-demat/prePrinted";
+stderr | src/pages/esign-demat/perimetreMetier/perimetreList.test.tsx > PerimetreMetierList > should show error alert if fetching data fails
+Error: API Error
+    at C:\dev\Front\consolepfel-refonte-front\src\pages\esign-demat\perimetreMetier\perimetreList.test.tsx:79:7
+    at file:///C:/dev/Front/consolepfel-refonte-front/node_modules/@vitest/runner/dist/index.js:146:14
+    at file:///C:/dev/Front/consolepfel-refonte-front/node_modules/@vitest/runner/dist/index.js:533:11
+    at runWithTimeout (file:///C:/dev/Front/consolepfel-refonte-front/node_modules/@vitest/runner/dist/index.js:61:7)
+    at runTest (file:///C:/dev/Front/consolepfel-refonte-front/node_modules/@vitest/runner/dist/index.js:986:17)
+    at runSuite (file:///C:/dev/Front/consolepfel-refonte-front/node_modules/@vitest/runner/dist/index.js:1142:15)
+    at runSuite (file:///C:/dev/Front/consolepfel-refonte-front/node_modules/@vitest/runner/dist/index.js:1142:15)
+    at runFiles (file:///C:/dev/Front/consolepfel-refonte-front/node_modules/@vitest/runner/dist/index.js:1199:5)
+    at startTests (file:///C:/dev/Front/consolepfel-refonte-front/node_modules/@vitest/runner/dist/index.js:1208:3)
+    at file:///C:/dev/Front/consolepfel-refonte-front/node_modules/vitest/dist/chunks/runBaseTests.D-Gcin7G.js:130:11
 
-import ProductFamilyList from "@/pages/esign-demat/product/family";
-import ProductLabelList from "@/pages/esign-demat/product/label";
-import TemplateList from "@/pages/esign-demat/template";
-import { LogsList } from "@/pages/logs";
-import { ApplicationsList } from "@/pages/pfel";
-import { SubrogationList } from "@/pages/subrogation";
-import { ProtectedRoute } from "./ProtectedRoute";
-import { UserRole } from "@/types/roleTypes";
-import {
-  createBrowserRouter,
-  Navigate,
-  RouterProvider,
-} from "react-router-dom";
-export const createProtectedRouter = (userRole: UserRole) => {
-  return createBrowserRouter([
-    {
-      path: "consolePfel",
-      element: (
-        <ProtectedRoute userRole={userRole}>
-          <Layout />
-        </ProtectedRoute>
-      ),
-      children: [
-        // Logs - accessible à tous
-        {
-          path: "logs/list",
-          element: <LogsList />,
-        },
-        // Routes protégées - accessible uniquement à l'admin
-        {
-          path: "pfel/list",
-          element: (
-            <ProtectedRoute userRole={userRole}>
-              <ApplicationsList />
-            </ProtectedRoute>
-          ),
-        },
-        {
-          path: "lettre/list",
-          element: (
-            <ProtectedRoute userRole={userRole}>
-              <LettreList />
-            </ProtectedRoute>
-          ),
-        },
-        {
-          path: "product",
-          children: [
-            {
-              path: "family/list",
-              element: (
-                <ProtectedRoute userRole={userRole}>
-                  <ProductFamilyList />
-                </ProtectedRoute>
-              ),
-            },
-            {
-              path: "label/list",
-              element: (
-                <ProtectedRoute userRole={userRole}>
-                  <ProductLabelList />
-                </ProtectedRoute>
-              ),
-            },
-          ],
-        },
-        {
-          path: "groupKey/list",
-          element: (
-            <ProtectedRoute userRole={userRole}>
-              <GroupeList />
-            </ProtectedRoute>
-          ),
-        },
-        {
-          path: "documentSettings",
-          children: [
-            {
-              path: "list",
-              element: (
-                <ProtectedRoute userRole={userRole}>
-                  <DocumentsList />
-                </ProtectedRoute>
-              ),
-            },
-            {
-              path: "setting/:id",
-              element: (
-                <ProtectedRoute userRole={userRole}>
-                  <FormSetting />
-                </ProtectedRoute>
-              ),
-            },
-          ],
-        },
-        {
-          path: "subrogation/list",
-          element: (
-            <ProtectedRoute userRole={userRole}>
-              <SubrogationList />
-            </ProtectedRoute>
-          ),
-        },
-        {
-          path: "businessPerimeter/list",
-          element: (
-            <ProtectedRoute userRole={userRole}>
-              <PerimetreMetierList />
-            </ProtectedRoute>
-          ),
-        },
-        {
-          path: "prePrinted/list",
-          element: (
-            <ProtectedRoute userRole={userRole}>
-              <PrePrintedList />
-            </ProtectedRoute>
-          ),
-        },
-        {
-          path: "template/list",
-          element: (
-            <ProtectedRoute userRole={userRole}>
-              <TemplateList />
-            </ProtectedRoute>
-          ),
-        },
-        // Redirection par défaut
-        {
-          path: "*",
-          element: <Navigate to="logs/list" replace />,
-        },
-      ],
+stderr | src/components/atoms/dateInput/dateInput.test.tsx > CustomInputDate > should call onChange when a date is selected
+Warning: `ReactDOMTestUtils.act` is deprecated in favor of `React.act`. Import `act` from `react` instead of `react-dom/test-utils`. See https://react.dev/warnings/react-dom-test-utils for more info.
+
+import { MemoryRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { render, screen, waitFor, act } from "@testing-library/react";
+import { describe, it, vi, expect, Mock } from "vitest";
+import { getPerimetresMetiers } from "@/api/apifunctions/perimetreMetier/perimetreMetier";
+import PerimetreMetierList from "./index";
+// Mock des fonctions et hooks
+vi.mock("@/stores/perimetreMetier/perimetreMetierStore", () => ({
+  default: vi.fn().mockReturnValue({
+    perimetresMetiers: {
+      businessPerimeters: [],
+      totalPages: 1,
     },
-    // Redirection racine
-    {
-      path: "/",
-      element: <Navigate to="consolePfel/logs/list" replace />,
-    },
-  ]);
-};
-// Pour utilisation dans AppRoutes.tsx
-export const AppRoutes: React.FC<{ userRole: UserRole }> = ({ userRole }) => {
-  const router = createProtectedRouter(userRole);
-  return <RouterProvider router={router} />;
-};
+    setPerimetreMetier: vi.fn(),
+  }),
+}));
+vi.mock("@/api/apifunctions/perimetreMetier/perimetreMetier", () => ({
+  getPerimetresMetiers: vi.fn(),
+  addPerimetreMetier: vi.fn(),
+  updatePerimetreMetier: vi.fn(),
+  deletePerimetreMetier: vi.fn(),
+}));
+describe("PerimetreMetierList", () => {
+  const createQueryClient = () =>
+    new QueryClient({
+      defaultOptions: {
+        queries: { retry: false },
+      },
+    });
+  it("should render the component with 'Ajouter un Périmètre Métier' button", async () => {
+    const queryClient = createQueryClient();
+    await act(async () => {
+      render(
+        <QueryClientProvider client={queryClient}>
+          <MemoryRouter>
+            <PerimetreMetierList />
+          </MemoryRouter>
+        </QueryClientProvider>
+      );
+    });
+    expect(screen.getByText("Ajouter un Périmètre Métier")).toBeInTheDocument();
+  });
+  it("should display 'Aucun Périmètre Métier trouvé' alert if the list is empty", async () => {
+    const queryClient = createQueryClient();
+    await act(async () => {
+      render(
+        <QueryClientProvider client={queryClient}>
+          <MemoryRouter>
+            <PerimetreMetierList />
+          </MemoryRouter>
+        </QueryClientProvider>
+      );
+    });
+    expect(
+      screen.getByText("Aucun Périmètre Métier trouvé.")
+    ).toBeInTheDocument();
+  });
+  it("should call the API to fetch data on mount", async () => {
+    const queryClient = createQueryClient();
+    (getPerimetresMetiers as Mock).mockResolvedValueOnce({
+      successData: { businessPerimeters: [], totalPages: 1 },
+    });
+    await act(async () => {
+      render(
+        <QueryClientProvider client={queryClient}>
+          <MemoryRouter>
+            <PerimetreMetierList />
+          </MemoryRouter>
+        </QueryClientProvider>
+      );
+    });
+    await waitFor(() => {
+      expect(getPerimetresMetiers).toHaveBeenCalled();
+    });
+  });
+  it("should show error alert if fetching data fails", async () => {
+    const queryClient = createQueryClient();
+    (getPerimetresMetiers as Mock).mockRejectedValueOnce(
+      new Error("API Error")
+    );
+    await act(async () => {
+      render(
+        <QueryClientProvider client={queryClient}>
+          <MemoryRouter>
+            <PerimetreMetierList />
+          </MemoryRouter>
+        </QueryClientProvider>
+      );
+    });
+    await waitFor(() => {
+      expect(
+        screen.getByText(
+          "Une erreur est survenue lors du chargement des Périmètres Métiers."
+        )
+      ).toBeInTheDocument();
+    });
+  });
+});
