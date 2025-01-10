98fc14eac2c8dc99026bd8e0370001262ecf51e8
zied <zied.elmiladi@gmail.com>
2025-01-10 18:21:53 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,62 +1,30 @@
-import Types from "@/Admin/type";
-import Button from "@/toolkit/Components/Form/Button";
-import Form from "@/toolkit/Components/Form/Form";
-import FormItem from "@/toolkit/Components/Form/FormItem";
-import Loader from "@/toolkit/Components/Loader";
-import { FC } from "react";
-
-interface Props {
-  className?: string;
-  onCancel?: (reason: any) => void;
-  onSave?: () => void;
-}
-export const HabilitationWithForm: FC<Props> = ({ className = "", onCancel = () => {}, onSave = () => {} }) => {
-  const cancelClick = () => {
-    console.log("object");
-  };
-  return (
-    <div >
-      <Form data-testid="agence-edit-form">
-        <fieldset >
-          <FormItem type="text" label="Nom" name="title" id="id-nom" requiredMessage="Le nom est obligatoire" required autoFocus />
-          <FormItem type="text" label="Prénom" name="title" id="id-nom" requiredMessage="Le nom est obligatoire" required autoFocus />
-          <FormItem
-            name="email"
-            type="email"
-            label="Adresse mail"
-            required={true}
-            id="id-mail"
-            maxLength={255}
-            pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$"
-            patternMessage="L'adresse mail doit être au format prenom.nom@axa.fr"
-            requiredMessage="L'adresse mail obligatoire"
-          />
-          <FormItem
-            type="text"
-            label="userNumber"
-            name="title"
-            id="id-userNumber"
-            requiredMessage="Le Matricule est obligatoire"
-            required
-            autoFocus
-          />
-            <Types className="" data-testid="types" />
-        </fieldset>
-        <div className="af-form-actions">
-          <Button
-            type="button"
-            classModifier="reverse"
-            onClick={cancelClick}
-            id="id-button-agence-cancel"
-            data-testid="id-button-agence-cancel"
-          >
-            Annuler
-          </Button>
-          <Button type="submit" classModifier="success" id="id-button-agence-save" data-testid="id-button-agence-save">
-            Enregistrer
-          </Button>
-        </div>
-      </Form>
+<div class="mb-4 border-b border-gray-200 dark:border-gray-700">
+    <ul class="flex flex-wrap -mb-px text-sm font-medium text-center" id="default-styled-tab" data-tabs-toggle="#default-styled-tab-content" data-tabs-active-classes="text-purple-600 hover:text-purple-600 dark:text-purple-500 dark:hover:text-purple-500 border-purple-600 dark:border-purple-500" data-tabs-inactive-classes="dark:border-transparent text-gray-500 hover:text-gray-600 dark:text-gray-400 border-gray-100 hover:border-gray-300 dark:border-gray-700 dark:hover:text-gray-300" role="tablist">
+        <li class="me-2" role="presentation">
+            <button class="inline-block p-4 border-b-2 rounded-t-lg" id="profile-styled-tab" data-tabs-target="#styled-profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Profile</button>
+        </li>
+        <li class="me-2" role="presentation">
+            <button class="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300" id="dashboard-styled-tab" data-tabs-target="#styled-dashboard" type="button" role="tab" aria-controls="dashboard" aria-selected="false">Dashboard</button>
+        </li>
+        <li class="me-2" role="presentation">
+            <button class="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300" id="settings-styled-tab" data-tabs-target="#styled-settings" type="button" role="tab" aria-controls="settings" aria-selected="false">Settings</button>
+        </li>
+        <li role="presentation">
+            <button class="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300" id="contacts-styled-tab" data-tabs-target="#styled-contacts" type="button" role="tab" aria-controls="contacts" aria-selected="false">Contacts</button>
+        </li>
+    </ul>
+</div>
+<div id="default-styled-tab-content">
+    <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="styled-profile" role="tabpanel" aria-labelledby="profile-tab">
+        <p class="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong class="font-medium text-gray-800 dark:text-white">Profile tab's associated content</strong>. Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to control the content visibility and styling.</p>
     </div>
-  );
-};
+    <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="styled-dashboard" role="tabpanel" aria-labelledby="dashboard-tab">
+        <p class="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong class="font-medium text-gray-800 dark:text-white">Dashboard tab's associated content</strong>. Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to control the content visibility and styling.</p>
+    </div>
+    <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="styled-settings" role="tabpanel" aria-labelledby="settings-tab">
+        <p class="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong class="font-medium text-gray-800 dark:text-white">Settings tab's associated content</strong>. Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to control the content visibility and styling.</p>
+    </div>
+    <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="styled-contacts" role="tabpanel" aria-labelledby="contacts-tab">
+        <p class="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong class="font-medium text-gray-800 dark:text-white">Contacts tab's associated content</strong>. Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to control the content visibility and styling.</p>
+    </div>
+</div>  je veux que tu fait  ebn scss  car pas besoin des telwinn  et uniquement 2 tab  habilitaion avec form et lautre habilitaion avec file et avec react tsx
