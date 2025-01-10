d163e819454389e9a52c41235e9c801aa9f246b1
zied <zied.elmiladi@gmail.com>
2025-01-10 17:20:55 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,137 +1,62 @@
-donne moi le fichiezr .csv  correspond a cette fonction : private List<UserModel> parseCsv(MultipartFile file) throws IOException {
-        List<UserModel> users = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] columns = line.split(";"); // Assuming CSV is comma-separated
-                if (columns.length >= 5) { // Ensure there are at least 4 columns
-                    UserModel user = new UserModel();
-                    String firstName = columns[0].trim();
-                    String lastName = columns[1].trim();
-                    String name =(StringUtils.isNoneBlank(firstName)?firstName.toUpperCase():
-                        "")+" "+(StringUtils.isNoneBlank(lastName)?lastName.toUpperCase():"");
-                    user.setName(name);
-                    user.setUserNumber(columns[2].trim());
-                    user.setEmail(columns[3].trim());
-                    String axaType = columns[4].trim();
-                    List<AuthoritiesEnum> authorities = Collections.singletonList(AuthoritiesEnum.fromValue(axaType));
-                    user.setAuthorities(createAuthorities(authorities));
-                    users.add(user);
-                }
-            }
-        }
-        return users;
-    }package fr.axa.pfel.ellipse.model.user;
+import Types from "@/Admin/type";
+import Button from "@/toolkit/Components/Form/Button";
+import Form from "@/toolkit/Components/Form/Form";
+import FormItem from "@/toolkit/Components/Form/FormItem";
+import Loader from "@/toolkit/Components/Loader";
+import { FC } from "react";
 
-import fr.axa.pfel.ellipse.constant.WorkspaceEnum;
-import fr.axa.pfel.ellipse.storage.domain.Container;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import lombok.Data;
-import org.apache.commons.lang3.StringUtils;
-
-@Data
-public class UserModel {
-
-    private Long id;
-    private String name;
-
-    private String email;
-    private String userNumber;
-    private String groupId;
-    private String axaType;
-    private List<PortfolioModel> portfolioModels = new ArrayList<>();
-    private List<AuthorityModel> authorities;
-    private List<WorkspaceModel> workspaceModelList;
-
-
-    public Long getWorkspaceId() {
-        if (workspaceModelList != null) {
-            return workspaceModelList.stream()
-                .filter(workspaceModel -> email.compareToIgnoreCase(
-                    workspaceModel.getName()) == 0
-                    && workspaceModel.getWorkspaceTypeModel() != null
-                    && workspaceModel.getWorkspaceTypeModel().getName().equals("WORKSPACE"))
-                .map(WorkspaceModel::getId).findFirst()
-                .orElse(null);
-        }
-
-        return null;
-    }
-
-
-    public String getDocumentPath(boolean isFs) {
-        return Container.DOCUMENT.getName() + (isFs ? ":/" : "/") + email;
-
-    }
-
-    public String getWorkspacePath(boolean isFs) {
-        return Container.WORKSPACE.getName() + (isFs ? ":/" : "/") + email;
-    }
-
-
-    public Set<String> getDefaultWorkspaces() {
-        if (this.authorities != null) {
-            return this.authorities.stream().map(authorityModel -> {
-                switch (authorityModel.getAuthority()) {
-                    case "ADMIN":
-                    case "AXA_PART":
-                        return Set.of(WorkspaceEnum.AXA_PART.getCode(),
-                            WorkspaceEnum.NATIONALE.getCode());
-                    case "AGT":
-                        return Set.of(WorkspaceEnum.NATIONALE.getCode());
-                    default:
-                        return new HashSet<String>();
-                }
-            }).reduce(new HashSet<>(), (l, l1) -> {
-                l.addAll(l1);
-                return l;
-            });
-        }
-
-        return Set.of(WorkspaceEnum.NATIONALE.getCode());
-
-    }
-
-    public boolean hasRole(String role) {
-        if (authorities != null) {
-            return authorities.stream()
-                .anyMatch(authorityModel -> authorityModel.getAuthority().equals(role));
-        }
-
-        return false;
-    }
-
-    public boolean hasWorkspace(Long id, String code) {
-        if (workspaceModelList != null && !workspaceModelList.isEmpty()) {
-            final var b = workspaceModelList.stream()
-                .anyMatch(workspaceModel -> workspaceModel.getId().equals(id));
-            var check = false;
-            if (!"PERSO".equals(code)) {
-                check = getDefaultWorkspaces().stream().anyMatch(s -> s.equals(code));
-            }
-
-            return b || check;
-        }
-        return false;
-    }
-
-    public boolean isAgent() {
-        if (StringUtils.isNotBlank(axaType)) {
-            return axaType.equals(AxaTypeEnum.AGENTS_A2P.getValue()) || axaType.equals(
-                AxaTypeEnum.AGENTS_A2P_ASSISTANTS.getValue()) || axaType.equals(
-                AxaTypeEnum.AGENTS_GENERAUX.getValue()) || axaType.equals(
-                AxaTypeEnum.AGENTS_COLLABORATEURS.getValue()) || axaType.equals(
-                AxaTypeEnum.AGENTS_MANDATAIRES.getValue());
-        }
-
-        return false;
-    }
-
-    public boolean isAdmin() {
-        return this.authorities != null && this.authorities.stream()
-            .anyMatch(authorityModel -> "ADMIN".equals(authorityModel.getAuthority()));
-    }
+interface Props {
+  className?: string;
+  onCancel?: (reason: any) => void;
+  onSave?: () => void;
 }
+export const HabilitationWithForm: FC<Props> = ({ className = "", onCancel = () => {}, onSave = () => {} }) => {
+  const cancelClick = () => {
+    console.log("object");
+  };
+  return (
+    <div >
+      <Form data-testid="agence-edit-form">
+        <fieldset >
+          <FormItem type="text" label="Nom" name="title" id="id-nom" requiredMessage="Le nom est obligatoire" required autoFocus />
+          <FormItem type="text" label="Prénom" name="title" id="id-nom" requiredMessage="Le nom est obligatoire" required autoFocus />
+          <FormItem
+            name="email"
+            type="email"
+            label="Adresse mail"
+            required={true}
+            id="id-mail"
+            maxLength={255}
+            pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$"
+            patternMessage="L'adresse mail doit être au format prenom.nom@axa.fr"
+            requiredMessage="L'adresse mail obligatoire"
+          />
+          <FormItem
+            type="text"
+            label="userNumber"
+            name="title"
+            id="id-userNumber"
+            requiredMessage="Le Matricule est obligatoire"
+            required
+            autoFocus
+          />
+            <Types className="" data-testid="types" />
+        </fieldset>
+        <div className="af-form-actions">
+          <Button
+            type="button"
+            classModifier="reverse"
+            onClick={cancelClick}
+            id="id-button-agence-cancel"
+            data-testid="id-button-agence-cancel"
+          >
+            Annuler
+          </Button>
+          <Button type="submit" classModifier="success" id="id-button-agence-save" data-testid="id-button-agence-save">
+            Enregistrer
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
+};
