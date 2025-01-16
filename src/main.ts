ec4efbadb38c928fa25fc2276ac21b5c900fbc87
zied <zied.elmiladi@gmail.com>
2025-01-16 10:41:53 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,1 @@
-PS C:\dev\Front\Ellipse> git pull
-error: cannot lock ref 'refs/remotes/origin/feature/DEMAT-15138': Unable to create 'C:/dev/Front/Ellipse/.git/refs/remotes/origin/feature/DEMAT-15138.lock': File exists.
-
-Another git process seems to be running in this repository, e.g.
-an editor opened by 'git commit'. Please make sure all processes
-are terminated then try again. If it still fails, a git process
-may have crashed in this repository earlier:
-remove the file manually to continue.
-From https://dev.azure.com/axafrance/PFEL/_git/Ellipse
- ! f0ca680..c1acfde  feature/DEMAT-15138 -> origin/feature/DEMAT-15138  (unable to update local ref)
-PS C:\dev\Front\Ellipse> 
+ jai besoin de la liste  utilisé  car par exemple pour ladmin  tu as  mis :ConsolePfel_Admin  alors que le service me retourne consolePfel_ADMIN
