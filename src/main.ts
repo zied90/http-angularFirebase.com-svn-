aed2cc835a5e9186b379e28a213f288d6693c855
zied <zied.elmiladi@gmail.com>
2025-01-17 16:58:39 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,48 @@
+import React from "react";
+interface FailedUser {
+  userNumber: string;
+  reason: string;
+}
+
+interface FailedUsersTableProps {
+  users: FailedUser[];
+  className?:string
+}
+
+export const FailedUsersTable: React.FC<FailedUsersTableProps> = ({ users,className }) => {
+  if (!users.length) return null;
+
+  return (
+    <div className={`HabilitationsFailedList ${className}`}>
+      <h3 className="failed-users-table__title">Utilisateurs en échec ({users.length})</h3>
+      <table className="table-panel">
+        <colgroup>
+          <col className="col-identifiant" />
+          <col className="col-raison" />
+
+        </colgroup>
+        <thead>
+          <tr>
+          <th>Identifiant</th>
+          <th>Raison de l'échec</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => (
+            <tr key={`${user.userNumber}-${index}`}>
+              <td>{user.userNumber}</td>
+              <td>{user.reason}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default FailedUsersTable;
+
+
 import FileUpload from "@/Admin/Components/FileUpload";
 import { FC, useState } from "react";
 import HabilitationsUsersList from "./HabilitationsUsersList";
@@ -8,6 +53,7 @@ import { adminHabilitationListDeployRoute } from "@/Api/ApiRoutes";
 import { useDelayApi } from "@/hooks/useApi";
 import Loader from "@/toolkit/Components/Loader";
 import StatusCard from "@/Admin/Components/StatusCard";
+import FailedUsersTable from "./HabilitationFailerUsersList/FailedUsers";
 
 export type HabilitationsFromListProps = {
   className?: string;
@@ -15,11 +61,12 @@ export type HabilitationsFromListProps = {
 
 const ALLOWED_FORMATS = [".xls", ".xlsx", ".csv", ".txt"];
 
-export const HabilitationsFromList: FC<HabilitationsFromListProps> = ({ className = "" }) => {
+export const HabilitationsFromList: FC<HabilitationsFromListProps> = ({ className = ""}) => {
   const [file, setFile] = useState<File | null>(null);
   const converter = new UserListConverter();
   const [users, setUsers] = useState<User[]>([]);
-  const { loaded, call: habiliterList,data } = useDelayApi(adminHabilitationListDeployRoute);
+  const { loaded, call: habiliterList, data } = useDelayApi(adminHabilitationListDeployRoute);
+  const [showResults, setShowResults] = useState<boolean>(false);
 
   const onChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files?.length) {
@@ -38,118 +85,112 @@ export const HabilitationsFromList: FC<HabilitationsFromListProps> = ({ classNam
     setUsers(users);
   };
 
-  const uploadHabilitations = async() => {
-   try{
-    const csv = converter.userListToCSV(users);
-     await habiliterList({csv});
-   }catch(e){
-    throw e
-   }
-    
+  const uploadHabilitations = async () => {
+    try {
+      const file = converter.userListToCSV(users);
+      await habiliterList({ file });
+      setShowResults(true);
+    } catch (e) {
+      throw e;
+    }
   };
-console.log(data,'oooiiii')
-
+  const handleReset = () => {
+    setShowResults(false);
+    setFile(null);
+    setUsers([]);
+   };
+   console.log("dd",data)
   return (
     <div className="HabilitationsFromList">
       <Loader loaded={loaded} />
-      <div className="row">
-        <div className="col">
-          <FileUpload
-            onChange={onChange}
-            file={file}
-            allowedFormats={ALLOWED_FORMATS}
-            explicationText={
-              <>
-                Téléchargez un fichier Excel, CSV, ou Texte pour importer des utilisateurs. <br />
-                Les fichiers CSV ou Texte, sont formatés avec des tabulations ou des virgules (,) ou (;) pour séparer les colonnes.
-              </>
-            }
-          />
+      {!showResults ? (
+        <>
+          <div className="row">
+            <div className="col">
+              <FileUpload
+                onChange={onChange}
+                file={file}
+                allowedFormats={ALLOWED_FORMATS}
+                explicationText={
+                  <>
+                    Téléchargez un fichier Excel, CSV, ou Texte pour importer des utilisateurs. <br />
+                    Les fichiers CSV ou Texte, sont formatés avec des tabulations ou des virgules (,) ou (;) pour séparer les colonnes.
+                  </>
+                }
+              />
+            </div>
+            <div className="col flex-col">
+              <button type="button" className="btn btn--primary" onClick={importFromClipboard}>
+                <i className="icon icon-clipboard"></i> Importer depuis le Presse-Papier
+              </button>
+              <button type="button" className="btn btn--success mt-a" onClick={uploadHabilitations}>
+                <i className="icon icon-arrow-up"></i> Envoyer les Habilitations
+              </button>
+            </div>
+          </div>
+          <div className="Habilitation--list mt-md">
+            <HabilitationsUsersList users={users} />
+          </div>
+        </>
+      ) : (
+        <div>
+          <div className="status-cards">
+            <StatusCard type="error" count={data.failedUsers.length} title="Échecs" description="habilitations échouées" />
+            <StatusCard type="success" count={data.successUsers.length} title="Succès" description="habilitations réussies" />
+          </div>
+          <FailedUsersTable users={data.failedUsers} />
+          <div className="text-center mt-md">
+            <button type="button" className="btn btn--secondary" onClick={handleReset}>
+              <i className="icon icon-arrow-left"></i> Importer un nouveau document
+            </button>
+          </div>
         </div>
-        <div className="col flex-col">
-          <button type="button" className="btn btn--primary" onClick={importFromClipboard}>
-            <i className="icon icon-clipboard"></i> Importer depuis le Presse-Papier
-          </button>
-          <button type="button" className="btn btn--success mt-a" onClick={uploadHabilitations}>
-            <i className="icon icon-arrow-up"></i> Envoyer les Habilitations
-          </button>
-        </div>
-      </div>
-      <div className="status-cards">
-<StatusCard
-       type="error"
-       count={3}
-       title="Échecs"
-       description="habilitations échouées"
-     />
-<StatusCard
-       type="success"
-       count={12}
-       title="Succès"
-       description="habilitations réussies"
-     />
-</div>
-      <div className="Habilitation--list mt-md">
-
-        <HabilitationsUsersList users={users} />
-      </div>
+      )}
     </div>
   );
 };
-  comment faire   afficher que  les 2 card   si ona data  et dans lexemple data represente retour api  {
-    "successUsers": [],
-    "failedUsers": [
-        {
-            "userNumber": "F146885",
-            "reason": "User already deployed"
-        },
-        {
-            "userNumber": "F146884",
-            "reason": "User already deployed"
-        },
-        {
-            "userNumber": "F146883",
-            "reason": "User already deployed"
-        },
-        {
-            "userNumber": "F146882",
-            "reason": "User already deployed"
-        },
-        {
-            "userNumber": "F146853",
-            "reason": "User already deployed"
-        },
-        {
-            "userNumber": "F146844",
-            "reason": "User already deployed"
-        },
-        {
-            "userNumber": "F146808",
-            "reason": "User already deployed"
-        },
-        {
-            "userNumber": "F146806",
-            "reason": "User already deployed"
-        },
-        {
-            "userNumber": "F146804",
-            "reason": "User already deployed"
-        },
-        {
-            "userNumber": "F146803",
-            "reason": "User already deployed"
-        },
-        {
-            "userNumber": "F146792",
-            "reason": "User already deployed"
-        },
-        {
-            "userNumber": "F146790",
-            "reason": "User already deployed"
-        },
-        {
-            "userNumber": "F146788",
-            "reason": "User already deployed"
-        }
-    ]
-} que je vais passer a la card cad resultat 
+
+import React from "react";
+interface FailedUser {
+  userNumber: string;
+  reason: string;
+}
+
+interface FailedUsersTableProps {
+  users: FailedUser[];
+  className?:string
+}
+
+export const FailedUsersTable: React.FC<FailedUsersTableProps> = ({ users,className }) => {
+  if (!users.length) return null;
+
+  return (
+    <div className={`HabilitationsFailedList ${className}`}>
+      <h3 className="failed-users-table__title">Utilisateurs en échec ({users.length})</h3>
+      <table className="table-panel">
+        <colgroup>
+          <col className="col-identifiant" />
+          <col className="col-raison" />
+
+        </colgroup>
+        <thead>
+          <tr>
+          <th>Identifiant</th>
+          <th>Raison de l'échec</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => (
+            <tr key={`${user.userNumber}-${index}`}>
+              <td>{user.userNumber}</td>
+              <td>{user.reason}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default FailedUsersTable; je veux      <HabilitationsUsersList users={users} /> et les 2 card soit dans un copsent   et le maittre a la place 
+
