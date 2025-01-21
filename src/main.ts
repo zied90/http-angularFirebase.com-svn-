6e27fab0d04e1532467f539b17df132c453d5085
zied <zied.elmiladi@gmail.com>
2025-01-21 15:24:17 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-pas question que je suis bloqué avec ton code , c'est juste  cote front  soit je travaille avec une  version  mocker soit j'Apple direct le backed  et aussi eviter des surprise  au cas ou   cest correct mon message a sami?
+PS C:\Users\B609EN> nvm -v
+1.2.2
+PS C:\Users\B609EN> nvm install 12.11.1
+Downloading node.js version 12.11.1 (64-bit)...
+Complete
+Downloading npm...
+Creating C:\Users\B609EN\AppData\Local\Temp\nvm-install-3861017411\temp
+
+Downloading npm version 6.11.3... Complete
+Installing npm v6.11.3...
+error installing 12.11.1: open C:\Users\B609EN\AppData\Local\Temp\nvm-npm-2593994530\npm-v6.11.3.zip: The system cannot find the file specified.
