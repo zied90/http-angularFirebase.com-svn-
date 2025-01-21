f76266ca782e040ce212f0469b83abecbe09b2f7
zied <zied.elmiladi@gmail.com>
2025-01-21 14:37:59 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,181 +1,114 @@
-import FileUpload from "@/Admin/Components/FileUpload";
-import { FC, useState } from "react";
-import HabilitationsUsersList from "./HabilitationsUsersList";
-import { User } from "@/Admin/types/User.type";
-import { UserListConverter } from "@/Admin/adminUtils/UserListConverter";
-import "./HabilitationsFromList.scss";
-import { adminHabilitationListDeployRoute } from "@/Api/ApiRoutes";
-import { useDelayApi } from "@/hooks/useApi";
-import Loader from "@/toolkit/Components/Loader";
-import HabilitationResults from "./HabilitationResult/HabilitationResults";
-import useModal from "@/toolkit/Components/Modal/useModal";
-
-export type HabilitationsFromListProps = {
-  className?: string;
-};
-
-const ALLOWED_FORMATS = [".xls", ".xlsx", ".csv", ".txt"];
-
-export const HabilitationsFromList: FC<HabilitationsFromListProps> = ({ className = ""}) => {
-  const [file, setFile] = useState<File | null>(null);
-  const converter = new UserListConverter();
-  const [users, setUsers] = useState<User[]>([]);
-  const { loaded, call: habiliterList, data } = useDelayApi(adminHabilitationListDeployRoute);
-  const [showResults, setShowResults] = useState<boolean>(false);
-  const onChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files?.length) {
-      let file: File = e.target.files[0];
-      setFile(file);
-      if (ALLOWED_FORMATS.includes(file.name.substring(file.name.lastIndexOf(".")))) {
-        const users = await converter.parseInput(file);
-        setUsers(users);
-      }
-    }
-  };
-
-  const importFromClipboard = async () => {
-    const clipboardData = await navigator.clipboard.readText();
-    const users = await converter.parseInput(clipboardData);
-    setUsers(users);
-  };
-
-  const uploadHabilitations = async () => {
-    try {
-      const file = converter.userListToCSV(users);
-      await habiliterList({ file });
-      setShowResults(true);
-    } catch (e) {
-      throw e;
-    }
-  };
-  const handleReset = () => {
-    setShowResults(false);
-    setFile(null);
-    setUsers([]);
-   };
-   console.log("dd",data)
-  return (
-    <div className="HabilitationsFromList">
-      <Loader loaded={loaded} />
-      {!showResults ? (
-        <>
-          <div className="row">
-            <div className="col">
-              <FileUpload
-                onChange={onChange}
-                file={file}
-                allowedFormats={ALLOWED_FORMATS}
-                explicationText={
-                  <>
-                    Téléchargez un fichier Excel, CSV, ou Texte pour importer des utilisateurs. <br />
-                    Les fichiers CSV ou Texte, sont formatés avec des tabulations ou des virgules (,) ou (;) pour séparer les colonnes.
-                  </>
-                }
-              />
-            </div>
-            <div className="col flex-col">
-              <button type="button" className="btn btn--primary" onClick={importFromClipboard}>
-                <i className="icon icon-clipboard"></i> Importer depuis le Presse-Papier
-              </button>
-              <button type="button" className="btn btn--success mt-a" onClick={uploadHabilitations}>
-                <i className="icon icon-arrow-up"></i> Envoyer les Habilitations
-              </button>
-            </div>
-          </div>
-          <div className="Habilitation--list mt-md">
-            <HabilitationsUsersList users={users} />
-          </div>
-        </>
-      ) : (
-        <HabilitationResults data={data} onReset={handleReset} />
-      )}
-    </div>
-  );
-};
-import React from "react";
-import StatusCard from "@/Admin/Components/StatusCard";
-import FailedUsersTable from "../HabilitationFailedUsersList/FailedUsers";
-
-export type HabilitationResultsProps = {
-  data: {
-    failedUsers: any[];
-    successUsers: any[];
-  };
-  onReset: () => void;
-};
-const HabilitationResults: React.FC<HabilitationResultsProps> = ({ data, onReset }) => {
-  return (
-    <div className="habilitation-results">
-      <button
-        className="btn btn--reverse"
-        onClick={onReset}
-        type="button"
-        id="id-address-add-new"
-        data-testid="Fermer-importer-un-nouveau-document"
-      >
-        Importer un nouveau document
-      </button>
-      <div className="status-cards">
-        <StatusCard type="error" count={data.failedUsers.length} title="Échecs" description="habilitations échouées" />
-        <StatusCard type="success" count={data.successUsers.length} title="Succès" description="habilitations réussies" />
-      </div>
-      <FailedUsersTable users={data.failedUsers} />
-    </div>
-  );
-};
-export default HabilitationResults;
-
-import React from "react";
-import "./FailerUsersList.scss";
-interface FailedUser {
-  userNumber: string;
-  reason: string;
-}
-
-interface FailedUsersTableProps {
-  users: FailedUser[];
-  className?:string
-}
-
-export const FailedUsersTable: React.FC<FailedUsersTableProps> = ({ users,className }) => {
-  if (!users.length) return null;
-
-  return (
-    <div className={`HabilitationsFailedList ${className}`}>
-      <h3 className="failed-users-table__title">Utilisateurs en échec ({users.length})</h3>
-      <table className="table-panel">
-        <colgroup>
-          <col className="col-identifiant" />
-          <col className="col-raison" />
-        </colgroup>
-        <thead>
-          <tr>
-          <th>Identifiant</th>
-          <th>Raison de l'échec</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user, index) => (
-            <tr key={`${user.userNumber}-${index}`}>
-              <td>{user.userNumber}</td>
-              <td>{user.reason}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-};
-
-.HabilitationsFailedList {
-  .table-panel {
-    thead th {
-      text-align: left;
-    }
-  }
-}
-
-
-
-export default FailedUsersTable;
-pour les user failer cest un tableau ca peux avoir 1000 user failer du cous je veux scroller sur cette liste 
+npm warn deprecated node-sass@4.14.1: Node Sass is no longer supported. Please use `sass` or `sass-embedded` instead.
+npm warn deprecated eslint@6.6.0: This version is no longer supported. Please see https://eslint.org/version-support for other options.
+npm warn deprecated core-js@1.2.7: core-js@<3.23.3 is no longer maintained and not recommended for usage due to the number of issues. Because of the V8 engine whims, feature detection in old core-js versions could cause a slowdown up to 100x even if nothing is polyfilled. Some versions have web compatibility issues. Please, upgrade your dependencies to the actual version of core-js.     
+npm warn deprecated core-js@2.6.11: core-js@<3.23.3 is no longer maintained and not recommended for usage due to the number of issues. Because of the V8 engine whims, feature detection in old core-js versions could cause a slowdown up to 100x even if nothing is polyfilled. Some versions have web compatibility issues. Please, upgrade your dependencies to the actual version of core-js.    
+npm warn deprecated core-js@2.6.10: core-js@<3.23.3 is no longer maintained and not recommended for usage due to the number of issues. Because of the V8 engine whims, feature detection in old core-js versions could cause a slowdown up to 100x even if nothing is polyfilled. Some versions have web compatibility issues. Please, upgrade your dependencies to the actual version of core-js.    
+npm warn deprecated core-js@2.6.11: core-js@<3.23.3 is no longer maintained and not recommended for usage due to the number of issues. Because of the V8 engine whims, feature detection in old core-js versions could cause a slowdown up to 100x even if nothing is polyfilled. Some versions have web compatibility issues. Please, upgrade your dependencies to the actual version of core-js.    
+npm warn deprecated core-js@3.2.1: core-js@<3.23.3 is no longer maintained and not recommended for usage due to the number of issues. Because of the V8 engine whims, feature detection in old core-js versions could cause a slowdown up to 100x even if nothing is polyfilled. Some versions have web compatibility issues. Please, upgrade your dependencies to the actual version of core-js.     
+npm warn deprecated @material-ui/core@4.11.4: Material UI v4 doesn't receive active development since September 2021. See the guide https://mui.com/material-ui/migration/migration-v4/ to upgrade 
+to v5.
+npm warn cleanup Failed to remove some directories [
+npm warn cleanup   [
+npm warn cleanup     'C:\\dev\\Front\\e-validation-front\\node_modules\\@axa-fr\\react-toolkit-form-input-date',
+npm warn cleanup     [Error: EPERM: operation not permitted, rmdir 'C:\dev\Front\e-validation-front\node_modules\@axa-fr\react-toolkit-form-input-date'] {
+npm warn cleanup       errno: -4048,
+npm warn cleanup       code: 'EPERM',
+npm warn cleanup       syscall: 'rmdir',
+npm warn cleanup       path: 'C:\\dev\\Front\\e-validation-front\\node_modules\\@axa-fr\\react-toolkit-form-input-date'
+npm warn cleanup     }
+npm warn cleanup   ],
+npm warn cleanup   [
+npm warn cleanup     'C:\\dev\\Front\\e-validation-front\\node_modules\\attr-accept',
+npm warn cleanup     [Error: EPERM: operation not permitted, rmdir 'C:\dev\Front\e-validation-front\node_modules\attr-accept\node_modules'] {
+npm warn cleanup       errno: -4048,
+npm warn cleanup       code: 'EPERM',
+npm warn cleanup       syscall: 'rmdir',
+npm warn cleanup       path: 'C:\\dev\\Front\\e-validation-front\\node_modules\\attr-accept\\node_modules'
+npm warn cleanup     }
+npm warn cleanup   ],
+npm warn cleanup   [
+npm warn cleanup     'C:\\dev\\Front\\e-validation-front\\node_modules\\browser-resolve',
+npm warn cleanup     [Error: EPERM: operation not permitted, rmdir 'C:\dev\Front\e-validation-front\node_modules\browser-resolve\node_modules\resolve\test\pathfilter\deep_ref\node_modules'] {    
+npm warn cleanup       errno: -4048,
+npm warn cleanup       code: 'EPERM',
+npm warn cleanup       syscall: 'rmdir',
+npm warn cleanup       path: 'C:\\dev\\Front\\e-validation-front\\node_modules\\browser-resolve\\node_modules\\resolve\\test\\pathfilter\\deep_ref\\node_modules'
+npm warn cleanup     }
+npm warn cleanup   ]
+npm warn cleanup ]
+npm error code 1
+npm error path C:\dev\Front\e-validation-front\node_modules\node-sass
+npm error command failed
+npm error command C:\windows\system32\cmd.exe /d /s /c node scripts/build.js
+npm error Building: C:\Users\B609EN\AppData\Local\Volta\tools\image\node\20.11.0\node.exe C:\dev\Front\e-validation-front\node_modules\node-gyp\bin\node-gyp.js rebuild --verbose --libsass_ext= --libsass_cflags= --libsass_ldflags= --libsass_library=
+npm error gyp info it worked if it ends with ok
+npm error gyp verb cli [
+npm error gyp verb cli   'C:\\Users\\B609EN\\AppData\\Local\\Volta\\tools\\image\\node\\20.11.0\\node.exe',
+npm error gyp verb cli   'C:\\dev\\Front\\e-validation-front\\node_modules\\node-gyp\\bin\\node-gyp.js',
+npm error gyp verb cli   'rebuild',
+npm error gyp verb cli   '--verbose',
+npm error gyp verb cli   '--libsass_ext=',
+npm error gyp verb cli   '--libsass_cflags=',
+npm error gyp verb cli   '--libsass_ldflags=',
+npm error gyp verb cli   '--libsass_library='
+npm error gyp verb cli ]
+npm error gyp info using node-gyp@3.8.0
+npm error gyp info using node@20.11.0 | win32 | x64
+npm error gyp verb command rebuild []
+npm error gyp verb command clean []
+npm error gyp verb clean removing "build" directory
+npm error gyp verb command configure []
+npm error gyp verb check python checking for Python executable "python2" in the PATH
+npm error gyp verb `which` failed Error: not found: python2
+npm error gyp verb `which` failed     at getNotFoundError (C:\dev\Front\e-validation-front\node_modules\which\which.js:13:12)
+npm error gyp verb `which` failed     at F (C:\dev\Front\e-validation-front\node_modules\which\which.js:68:19)
+npm error gyp verb `which` failed     at E (C:\dev\Front\e-validation-front\node_modules\which\which.js:80:29)
+npm error gyp verb `which` failed     at C:\dev\Front\e-validation-front\node_modules\which\which.js:89:16
+npm error gyp verb `which` failed     at C:\dev\Front\e-validation-front\node_modules\isexe\index.js:42:5
+npm error gyp verb `which` failed     at C:\dev\Front\e-validation-front\node_modules\isexe\windows.js:36:5
+npm error gyp verb `which` failed     at FSReqCallback.oncomplete (node:fs:201:21)
+npm error gyp verb `which` failed  python2 Error: not found: python2
+npm error gyp verb `which` failed     at getNotFoundError (C:\dev\Front\e-validation-front\node_modules\which\which.js:13:12)
+npm error gyp verb `which` failed     at F (C:\dev\Front\e-validation-front\node_modules\which\which.js:68:19)
+npm error gyp verb `which` failed     at E (C:\dev\Front\e-validation-front\node_modules\which\which.js:80:29)
+npm error gyp verb `which` failed     at C:\dev\Front\e-validation-front\node_modules\which\which.js:89:16
+npm error gyp verb `which` failed     at C:\dev\Front\e-validation-front\node_modules\isexe\index.js:42:5
+npm error gyp verb `which` failed     at C:\dev\Front\e-validation-front\node_modules\isexe\windows.js:36:5
+npm error gyp verb `which` failed     at FSReqCallback.oncomplete (node:fs:201:21) {
+npm error gyp verb `which` failed   code: 'ENOENT'
+npm error gyp verb `which` failed }
+npm error gyp verb check python checking for Python executable "python" in the PATH
+npm error gyp verb `which` failed Error: not found: python
+npm error gyp verb `which` failed     at getNotFoundError (C:\dev\Front\e-validation-front\node_modules\which\which.js:13:12)
+npm error gyp verb `which` failed     at F (C:\dev\Front\e-validation-front\node_modules\which\which.js:68:19)
+npm error gyp verb `which` failed     at E (C:\dev\Front\e-validation-front\node_modules\which\which.js:80:29)
+npm error gyp verb `which` failed     at C:\dev\Front\e-validation-front\node_modules\which\which.js:89:16
+npm error gyp verb `which` failed     at C:\dev\Front\e-validation-front\node_modules\isexe\index.js:42:5
+npm error gyp verb `which` failed     at FSReqCallback.oncomplete (node:fs:201:21)
+npm error gyp verb `which` failed  python Error: not found: python
+npm error gyp verb `which` failed     at getNotFoundError (C:\dev\Front\e-validation-front\node_modules\which\which.js:13:12)
+npm error gyp verb `which` failed     at F (C:\dev\Front\e-validation-front\node_modules\which\which.js:68:19)
+npm error gyp verb `which` failed     at E (C:\dev\Front\e-validation-front\node_modules\which\which.js:80:29)
+npm error gyp verb `which` failed     at C:\dev\Front\e-validation-front\node_modules\which\which.js:89:16
+npm error gyp verb `which` failed     at C:\dev\Front\e-validation-front\node_modules\isexe\index.js:42:5
+npm error gyp verb `which` failed     at C:\dev\Front\e-validation-front\node_modules\isexe\windows.js:36:5
+npm error gyp verb `which` failed     at FSReqCallback.oncomplete (node:fs:201:21) {
+npm error gyp verb `which` failed   code: 'ENOENT'
+npm error gyp verb `which` failed }
+npm error gyp verb could not find "python". checking python launcher
+npm error gyp verb could not find "python". guessing location
+npm error gyp verb ensuring that file exists: C:\Python27\python.exe
+npm error gyp ERR! configure error
+npm error gyp ERR! stack Error: Can't find Python executable "python", you can set the PYTHON env variable.
+npm error gyp ERR! stack     at PythonFinder.failNoPython (C:\dev\Front\e-validation-front\node_modules\node-gyp\lib\configure.js:484:19)
+npm error gyp ERR! stack     at PythonFinder.<anonymous> (C:\dev\Front\e-validation-front\node_modules\node-gyp\lib\configure.js:509:16)
+npm error gyp ERR! stack     at callback (C:\dev\Front\e-validation-front\node_modules\graceful-fs\polyfills.js:295:20)
+npm error gyp ERR! stack     at FSReqCallback.oncomplete (node:fs:201:21)
+npm error gyp ERR! System Windows_NT 10.0.19045
+npm error gyp ERR! command "C:\\Users\\B609EN\\AppData\\Local\\Volta\\tools\\image\\node\\20.11.0\\node.exe" "C:\\dev\\Front\\e-validation-front\\node_modules\\node-gyp\\bin\\node-gyp.js" "rebuild" "--verbose" "--libsass_ext=" "--libsass_cflags=" "--libsass_ldflags=" "--libsass_library="
+npm error gyp ERR! cwd C:\dev\Front\e-validation-front\node_modules\node-sass
+npm error gyp ERR! node -v v20.11.0
+npm error gyp ERR! node-gyp -v v3.8.0
+npm error gyp ERR! not ok
+npm error Build failed with error code: 1
+npm error A complete log of this run can be found in: C:\Users\B609EN\AppData\Local\npm-cache\_logs\2025-01-21T13_29_53_701Z-debug-0.log
