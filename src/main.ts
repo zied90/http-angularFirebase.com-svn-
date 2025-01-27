f889656c172c4674792d2c63463b88bd2d785057
zied <zied.elmiladi@gmail.com>
2025-01-27 14:50:54 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,101 +1,66 @@
-import { FC, useMemo, useState } from "react";
-import FormItem from "@/toolkit/Components/Form/FormItem";
-import { useDelayApi } from "@/hooks/useApi";
-import { adminProfilesRoute } from "@/Api/ApiRoutes";
-interface Props {
- className?: string;
- onChange?: (value: string) => void;
- value?: string;
-}
-const Profiles: FC<Props> = ({ className = "", onChange, value }) => {
-    const { call: loadAllProfiles } = useDelayApi(adminProfilesRoute);
- const [profiles] = useState<any>([
-   { "id": "ADMIN", name: "ADMIN" },
-   { "id": "AGT", name: "AGT" }
- ]);
- const profileDatas = useMemo(
-   () => [
-     { label: "Sélectionnez un profile", value: "" },
-     ...profiles.map(({ name, id }: any) => ({ label: name, value: id }))
-   ],
-   [profiles]
- );
- const handleChange = (event: React.ChangeEvent<HTMLFormElement>) => {
-   onChange?.(event.target.value);
- };
- return profiles && profiles.length ? (
-<FormItem
-     id="id-profile"
-     labelStyle={className}
-     type="select"
-     label="Profile"
-     name="authorities"
-     placeholder="Sélectionner un Profile"
-     required={true}
-     visibleValue={profiles.length === 1 ? profiles[0].name : undefined}
-     value={value || (profiles.length === 1 ? profiles[0].id : "")}
-     datas={profileDatas}
-     onChange={handleChange}
-   />
- ) : null;
-};
-export default Profiles;   loadAllProfilesretourne la liste des profiler commentr faitre pour  se debarsser  [
-   { "id": "ADMIN", name: "ADMIN" },
-   { "id": "AGT", name: "AGT" }
- ])
- et sur un autre exmple jai trouver ca  pour la gestion des branches
-
-import { FC, useEffect, useMemo, useState } from "react";
-import { useDelayApi } from "@/hooks/useApi";
-import { allBranchesRoute, templateBrancheRoute } from "@/Api/ApiRoutes";
+import { FC,useMemo, useState } from "react";
 import FormItem from "@/toolkit/Components/Form/FormItem";
 
 interface Props {
   className?: string;
-  id?: string | undefined;
 }
+// TODO: Appel API dès que l'endpoint est prêt
+const Workspaces: FC<Props> = ({ className = "" }) => {
 
-const Branches: FC<Props> = ({ className = "", id }) => {
-  const { call: loadTemplateBranches } = useDelayApi(templateBrancheRoute, { id });
-  const { call: loadAllBranches } = useDelayApi(allBranchesRoute);
-  const [branches, setBranches] = useState<any>(null);
-  const branchesDatas = useMemo(
-    () => [{ label: "Sélectionnez une branche", value: "" }, ...(branches || [])?.map(({ name, id }: any) => ({ label: name, value: id }))],
-    [branches]
+  const [workspaces] = useState<any>([{"id":1,name:"Nationale"},{"id":2,name:"Axa part"}]);
+  const workspaceDatas = useMemo(
+    () => [{ label: "Sélectionnez un workspace", value: "" }, ...(workspaces || [])?.map(({ name, id }: any) => ({ label: name, value: id }))],
+    [workspaces]
   );
 
-  useEffect(() => {
-    (async () => {
-      let hasBranches = false;
-      if (id) {
-        const templateBranches = await loadTemplateBranches({ id });
-        if (templateBranches && templateBranches.length) {
-          hasBranches = true;
-          setBranches(templateBranches);
-        }  
-      }
-      if(!hasBranches) {
-        const allBranches = await loadAllBranches();
-        setBranches(allBranches);
-      }
-    })();
-  }, [id, loadAllBranches, loadTemplateBranches]);
-
-  return branches && branches.length ? (
+  return workspaces && workspaces.length ? (
     <FormItem
-      id="id-courrier-branche"
+      id="id-workspace"
       labelStyle={className}
-      type={branches.length === 1 ? "viewhidden" : "select"}
-      label="Branche"
-      name="branch"
-      placeholder="Sélectionner une branche"
+      type="select"
+      label="Workspace"
+      name="workspaceId"
+      placeholder="Sélectionner un Workspace"
       required={true}
-      visibleValue={branches.length === 1 ? branches[0].name : undefined}
-      value={branches.length === 1 ? branches[0].id : undefined}
-      datas={branchesDatas}
+      visibleValue={workspaces.length === 1 ? workspaces[0].name : undefined}
+      value={workspaces.length === 1 ? workspaces[0].id : undefined}
+      datas={workspaceDatas}
     />
   ) : null;
 };
 
-export default Branches;
+export default Workspaces;
 
+
+import { FC, useMemo, useState } from "react";
+import FormItem from "@/toolkit/Components/Form/FormItem";
+
+interface Props {
+  className?: string;
+}
+// TODO: Appel API dès que l'endpoint est prêt
+const Types: FC<Props> = ({ className = "" }) => {
+
+  const [types] = useState<any>([{"id":"CTR",name:"Contrat"},{"id":"CLT",name:"Client"}]);
+  const typeDatas = useMemo(
+    () => [{ label: "Sélectionnez un type", value: "" }, ...(types || [])?.map(({ name, id }: any) => ({ label: name, value: id }))],
+    [types]
+  );
+
+  return types && types.length ? (
+    <FormItem
+      id="id-Type-template"
+      labelStyle={className}
+      type="select"
+      label="Type"
+      name="type"
+      placeholder="Sélectionner un Type"
+      required={true}
+      visibleValue={types.length === 1 ? types[0].name : undefined}
+      value={types.length === 1 ? types[0].id : undefined}
+      datas={typeDatas}
+    />
+  ) : null;
+};
+
+export default Types;  fait le meme proncipe pour ces 2 compsent
