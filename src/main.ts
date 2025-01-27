18e80795737499773a59db5350a2f3be3db4ac91
zied <zied.elmiladi@gmail.com>
2025-01-27 15:49:29 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,88 +1,49 @@
-import { useAxios } from "@/hooks/useAxios";
-import { useOidcAccessToken } from "@/oidc/useOidc";
-import { useContext } from "react";
-import { AlertTypeEnum } from "@/toolkit/Components/Alert/Alert.type";
-import { GlobalAlertContext } from "@/toolkit/Components/GlobalAlert/GlobalAlertContext";
-import ApiRoute from "@/types/ApiRoute.type";
-import AxiosOptions from "@/types/AxiosOptions.type";
-import { DEFAULT_API_ERROR_MESSAGE, getAlertMessage, simplifyParams } from "@/Utils/ApiUtils";
+import { FC, useEffect, useMemo, useState } from "react";
+import FormItem from "@/toolkit/Components/Form/FormItem";
+import { useDelayApi } from "@/hooks/useApi";
+import { allProfilesRoute } from "@/Api/ApiRoutes";
+import { Profile } from "../types/Profile.type";
+interface Props {
+  className?: string;
+  onChange?: (value: string) => void;
+  value?: string;
+}
+const Profiles: FC<Props> = ({ className = "", onChange, value }) => {
+  const { call: loadAllProfiles } = useDelayApi(allProfilesRoute);
+  const [profiles, setProfiles] = useState<Profile[]>([]);
+  const profileDatas = useMemo(
+    () => [{ label: "Sélectionnez un profile", value: "" }, ...profiles.map(({ name, id }: Profile) => ({ label: name, value: id }))],
+    [profiles]
+  );
 
-export const useApi = (route: ApiRoute, localParams: any = null, axiosOptions: AxiosOptions = {}) => {
-  const { accessToken } = useOidcAccessToken();
-  const { addAlert } = useContext(GlobalAlertContext);
-
-  const accessTokenHeader = accessToken
-    ? {
-        Authorization: `Bearer ${accessToken}`,
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await loadAllProfiles();
+        setProfiles(response);
+      } catch (error) {
+        console.error("Erreur lors du chargement des profils", error);
+        setProfiles([]);
       }
-    : {};
-  const config = {
-    headers: {
-      ...accessTokenHeader,
-    },
+    })();
+  }, [loadAllProfiles]);
+  const handleChange = (event: React.ChangeEvent<HTMLFormElement>) => {
+    onChange?.(event.target.value);
   };
-
-  let { currentRoute, axiosParams } = simplifyParams(route, localParams, localParams);
-
-  const newAxiosOptions = { ...axiosOptions, ...currentRoute.axiosOptions };
-  newAxiosOptions.routeId = currentRoute.id;
-  newAxiosOptions.data = axiosParams.datas;
-  newAxiosOptions.params = axiosParams.params;
-  newAxiosOptions.method = currentRoute.method;
-  newAxiosOptions.cache = currentRoute.cache || false;
-  newAxiosOptions.cacheMethods = currentRoute.method ? [currentRoute.method] : ["GET"];
-  newAxiosOptions.ttl = currentRoute.ttl || 0;
-  newAxiosOptions.headers = config.headers;
-
-  const updateLocalDataFn = (data: any) => {
-    updateLocalData(data);
-  };
-
-  const {
-    cancel,
-    data,
-    error,
-    loaded,
-    loadedOnce,
-    call: callAxios,
-    clearError,
-    updateLocalData,
-  } = useAxios(currentRoute.path, newAxiosOptions as AxiosOptions);
-
-  const call = async (params?: any): Promise<any> => {
-    let { currentRoute, axiosParams } = simplifyParams(route, params, localParams);
-
-    try {
-      const result = await callAxios(axiosParams, currentRoute);
-
-      if (route.alertSuccess) {
-        addAlert({
-          // @ts-ignore
-          message: route.alertSuccess.message,
-          type: AlertTypeEnum.success,
-        });
-      }
-      return route.responseConverter ? route.responseConverter(result) : result;
-    } catch (e: any) {
-      route.alertError
-        ? addAlert({
-            message: getAlertMessage(route.alertError, e.response?.data),
-            // @ts-ignore
-            type: route.alertError.type || AlertTypeEnum.error,
-          })
-        : addAlert({
-            message: e.response?.data?.message || DEFAULT_API_ERROR_MESSAGE,
-            type: AlertTypeEnum.error,
-          });
-
-      console.error(e);
-      throw e;
-    }
-  };
-
-  return { cancel, data, error, loaded, loadedOnce, call, clearError, updateLocalData: updateLocalDataFn };
+  return profiles && profiles.length ? (
+    <FormItem
+      id="id-profile"
+      labelStyle={className}
+      type="select"
+      label="Profile"
+      name="authorities"
+      placeholder="Sélectionner un Profile"
+      required={true}
+      visibleValue={profiles.length === 1 ? profiles[0].name : undefined}
+      value={value || (profiles.length === 1 ? profiles[0].id : "")}
+      datas={profileDatas}
+      onChange={handleChange}
+    />
+  ) : null;
 };
-
-export const useDelayApi = (route: ApiRoute, params: any = null, axiosOptions: AxiosOptions = {}) => {
-  return useApi(route, params, { ...axiosOptions, atStart: false });
-};
+export default Profiles;
