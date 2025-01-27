da78029e31aa3b94dc381a2881411abb2ba9885b
zied <zied.elmiladi@gmail.com>
2025-01-27 14:37:49 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -43,3 +43,59 @@ export default Profiles;   loadAllProfilesretourne la liste des profiler comment
    { "id": "ADMIN", name: "ADMIN" },
    { "id": "AGT", name: "AGT" }
  ])
+ et sur un autre exmple jai trouver ca  pour la gestion des branches
+
+import { FC, useEffect, useMemo, useState } from "react";
+import { useDelayApi } from "@/hooks/useApi";
+import { allBranchesRoute, templateBrancheRoute } from "@/Api/ApiRoutes";
+import FormItem from "@/toolkit/Components/Form/FormItem";
+
+interface Props {
+  className?: string;
+  id?: string | undefined;
+}
+
+const Branches: FC<Props> = ({ className = "", id }) => {
+  const { call: loadTemplateBranches } = useDelayApi(templateBrancheRoute, { id });
+  const { call: loadAllBranches } = useDelayApi(allBranchesRoute);
+  const [branches, setBranches] = useState<any>(null);
+  const branchesDatas = useMemo(
+    () => [{ label: "Sélectionnez une branche", value: "" }, ...(branches || [])?.map(({ name, id }: any) => ({ label: name, value: id }))],
+    [branches]
+  );
+
+  useEffect(() => {
+    (async () => {
+      let hasBranches = false;
+      if (id) {
+        const templateBranches = await loadTemplateBranches({ id });
+        if (templateBranches && templateBranches.length) {
+          hasBranches = true;
+          setBranches(templateBranches);
+        }  
+      }
+      if(!hasBranches) {
+        const allBranches = await loadAllBranches();
+        setBranches(allBranches);
+      }
+    })();
+  }, [id, loadAllBranches, loadTemplateBranches]);
+
+  return branches && branches.length ? (
+    <FormItem
+      id="id-courrier-branche"
+      labelStyle={className}
+      type={branches.length === 1 ? "viewhidden" : "select"}
+      label="Branche"
+      name="branch"
+      placeholder="Sélectionner une branche"
+      required={true}
+      visibleValue={branches.length === 1 ? branches[0].name : undefined}
+      value={branches.length === 1 ? branches[0].id : undefined}
+      datas={branchesDatas}
+    />
+  ) : null;
+};
+
+export default Branches;
+
