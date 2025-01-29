656cbe10c8e98c19a9230ab8a0353bcaa8d0e84e
zied <zied.elmiladi@gmail.com>
2025-01-29 10:19:59 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,49 +1,136 @@
-import { FC, useEffect, useMemo, useState } from "react";
-import FormItem from "@/toolkit/Components/Form/FormItem";
+import { putInfosUserRoute } from "@/Api/ApiRoutes";
 import { useDelayApi } from "@/hooks/useApi";
-import { allProfilesRoute } from "@/Api/ApiRoutes";
-import { Profile } from "../types/Profile.type";
+import { useOidcUser } from "../useOidc";
+import { FC, useEffect, useState } from "react";
+import "./SendUserInfos.scss";
+
 interface Props {
   className?: string;
-  onChange?: (value: string) => void;
-  value?: string;
+  children?: React.ReactNode;
 }
-const Profiles: FC<Props> = ({ className = "", onChange, value }) => {
-  const { call: loadAllProfiles } = useDelayApi(allProfilesRoute);
-  const [profiles, setProfiles] = useState<Profile[]>([]);
-  const profileDatas = useMemo(
-    () => [{ label: "Sélectionnez un profile", value: "" }, ...profiles.map(({ name, id }: Profile) => ({ label: name, value: id }))],
-    [profiles]
-  );
+
+const SendUserInfos: FC<Props> = ({ children }) => {
+  const { oidcUser,  } = useOidcUser();
+  const { email, axa_uid_racf, name, axa_uid_rdu, axa_type } = (oidcUser as any) || {};
+  const { call, loaded,data } = useDelayApi(putInfosUserRoute);
+  const [userAllowed, setUserAllowed] = useState(false);
 
   useEffect(() => {
     (async () => {
       try {
-        const response = await loadAllProfiles();
-        setProfiles(response);
+      const e=await call({ email, userNumber: axa_uid_racf, name, axaUiRdu: axa_uid_rdu, axaType: axa_type });
+      console.log(e,"ooooo")
+      console.log(oidcUser,"kkkkkkkkkkkk")
+        setUserAllowed(true);
       } catch (error) {
-        console.error("Erreur lors du chargement des profils", error);
-        setProfiles([]);
+        console.error("Error while sending user infos to Maam", error);
       }
     })();
-  }, [loadAllProfiles]);
-  const handleChange = (event: React.ChangeEvent<HTMLFormElement>) => {
-    onChange?.(event.target.value);
-  };
-  return profiles && profiles.length ? (
-    <FormItem
-      id="id-profile"
-      labelStyle={className}
-      type="select"
-      label="Profile"
-      name="authorities"
-      placeholder="Sélectionner un Profile"
-      required={true}
-      visibleValue={profiles.length === 1 ? profiles[0].name : undefined}
-      value={value || (profiles.length === 1 ? profiles[0].id : "")}
-      datas={profileDatas}
-      onChange={handleChange}
-    />
-  ) : null;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [email, axa_uid_racf, name, axa_type]);
+  return (
+    <>
+      {loaded ? (
+        userAllowed ? (
+          children
+        ) : (
+          <div className="bigCenteredMessage">Vous n'avez pas accès à Ellipse, veuillez contacter votre administrateur.</div>
+        )
+      ) : (
+        <div className="bigCenteredMessage">Chargement de Ellipse en cours</div>
+      )}
+    </>
+  );
 };
-export default Profiles;
+
+export default SendUserInfos;     voic limplementation et que je ne pas la main cest just pour tu commprenne import { OidcClient, type OidcUserInfo } from '@axa-fr/oidc-client';
+import { useEffect, useState } from 'react';
+
+export enum OidcUserStatus {
+  Unauthenticated = 'Unauthenticated',
+  Loading = 'Loading user',
+  Loaded = 'User loaded',
+  LoadingError = 'Error loading user',
+}
+
+export type OidcUser<T extends OidcUserInfo = OidcUserInfo> = {
+  user: T | null;
+  status: OidcUserStatus;
+};
+
+export const useOidcUser = <T extends OidcUserInfo = OidcUserInfo>(
+  configurationName = 'default',
+  demonstrating_proof_of_possession = false,
+) => {
+  const oidc = OidcClient.get(configurationName);
+  const user = oidc.userInfo<T>();
+  const [oidcUser, setOidcUser] = useState<OidcUser<T>>({
+    user: user,
+    status: user ? OidcUserStatus.Loaded : OidcUserStatus.Unauthenticated,
+  });
+  const [oidcUserId, setOidcUserId] = useState<number>(user ? 1 : 0);
+  const [oidcPreviousUserId, setPreviousOidcUserId] = useState<number>(user ? 1 : 0);
+
+  useEffect(() => {
+    const oidc = OidcClient.get(configurationName);
+    let isMounted = true;
+    if (oidc && oidc.tokens) {
+      const isCache = oidcUserId === oidcPreviousUserId;
+      if (isCache && oidc.userInfo<T>()) {
+        return;
+      }
+      setOidcUser({ ...oidcUser, status: OidcUserStatus.Loading });
+      oidc
+        .userInfoAsync(!isCache, demonstrating_proof_of_possession)
+        .then(info => {
+          if (isMounted) {
+            // @ts-ignore
+            setOidcUser({ user: info, status: OidcUserStatus.Loaded });
+          }
+        })
+        .catch(() => setOidcUser({ ...oidcUser, status: OidcUserStatus.LoadingError }));
+      setPreviousOidcUserId(oidcUserId);
+    } else {
+      setOidcUser({ user: null, status: OidcUserStatus.Unauthenticated });
+    }
+    const newSubscriptionId = oidc.subscribeEvents((name: string) => {
+      if (
+        name === OidcClient.eventNames.logout_from_another_tab ||
+        name === OidcClient.eventNames.logout_from_same_tab
+      ) {
+        if (isMounted) {
+          setOidcUser({ user: null, status: OidcUserStatus.Unauthenticated });
+        }
+      }
+    });
+    return () => {
+      isMounted = false;
+      oidc.removeEventSubscription(newSubscriptionId);
+    };
+  }, [oidcUserId]);
+
+  const reloadOidcUser = () => {
+    setOidcUserId(oidcUserId + 1);
+  };
+
+  return { oidcUser: oidcUser.user, oidcUserLoadingState: oidcUser.status, reloadOidcUser };
+};
+ et voila le  console.log(e,"ooooo"):{
+    "authority": [
+        "ADMIN"
+    ],
+    "id": 263,
+    "email": "zied.miladi.intm@axa.fr",
+    "userNumber": "S875170",
+    "axaType": "2",
+    "name": "MILADI Zied",
+    "portfolios": []
+} et ca cest console.log(oidcUser,"kkkkkkkkkkkk"){
+    "sub": "B609EN",
+    "name": "MILADI Zied",
+    "axa_type": "2",
+    "given_name": "Zied",
+    "family_name": "MILADI",
+    "email": "zied.miladi.intm@axa.fr",
+    "axa_uid_racf": "S875170"
+}   ma demande cest de ajouter  la valeur  ADMIN dans  oidcUser  
