7445057ca96d0d5d01bb8cd7a34cf6798315673b
zied <zied.elmiladi@gmail.com>
2025-02-06 14:55:57 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,224 +1,230 @@
-// import { putInfosUserRoute } from "Api/ApiRoutes";
-// import { useDelayApi } from "hooks/useApi";
-import { useOidcAccessToken, useOidcUser } from "../useOidc";
-import React, { FC, useEffect } from "react";
-import "./SendUserInfos.scss";
+import React, { useEffect } from "react";
 import { useMutation } from "react-query";
-import { getUserInfos } from "@/api/apifunctions/user";
-//import { HTTP } from "@/api/config/apiHandler";
+import Loader from "@axa-fr/react-toolkit-loader";
+import configStore from "@/stores/configStore";
+import { fusionDocumentsWithEsignTreatment } from "@/api/apifunctions/fusionDocumentsWithEsignTreatment";
+import { preprintedList } from "@/api/apifunctions/preprintedList";
+import { subscribers } from "@/api/apifunctions/subscribers";
+
+import { updateEsignDocSent } from "@/api/apifunctions/updateEsignDocSent";
+import { IDocument } from "@/types/documents";
+import { addLeadingZeros, getDomainForUrl, getEsignUrl } from "@/utils/index";
+import { documentsByActs } from "@/api/apifunctions/documentsByActs";
+
+import {
+  allActsAreEsign,
+  allDocAreIL101,
+  allDocsAreEsign,
+  allDocsHaveSameClientNumber,
+  allDocsHaveSameContractNumber,
+  allDocsHaveSameSubscriberNumber,
+  getActsNotEsign,
+  getDocsEsign,
+  getDocsNotEsign,
+  getDocsToDemat,
+  getDocumentsIds,
+} from "@/utils/docUtils";
 import userStore from "@/stores/userStore";
-import { setApiToken } from "@/api/config/apiHandler";
-import useFilters from "@/pages/Filters/useFilters";
+import { showDematNotEsignModal } from "./modals/modalNotEsign";
+import { showDematModal } from "./modals/modalDemat";
+import { showModalNotSameNumber } from "./modals/modalNotSameNumber";
+import { showModalPrePrint } from "./modals/modalPrePrint";
+import { ged } from "@/api/apifunctions/ged";
+import { ResolverPropsType } from "@/types/ResolverDataType";
+import useDocFilters from "@/hooks/useDocFilters";
+import { mergeGedWithDocs } from "@/utils/getEsignUrl";
+import { useAlert } from "@/components/GlobalAlert/useAlert";
 
-interface Props {
-  className?: string;
-  children?: React.ReactNode;
-  //onSuccess: (successData: any) => void;
-}
+const EsignResolver: React.FC<ResolverPropsType> = ({ data, resolverSuccess, resolverCancel }) => {
+  const profile = userStore()?.user?.profile || { esign: false, demat: false };
+  const { alertError } = useAlert();
 
-const SendUserInfos: FC<Props> = ({ children }) => {
-  const { setUser, setAuthToken } = userStore();
-  const { filtersInUrl, setFilters } = useFilters();
+  const { docFilters } = useDocFilters();
 
+  const { config } = configStore(); 
 
-  const { oidcUser } = useOidcUser();
-  const { email, axa_uid_racf, name, axa_type, axa_uid_rdu } = (oidcUser as any) || {};
-  const [userAllowed, setUserAllowed] = React.useState(false);
-  const { accessToken } = useOidcAccessToken();
+  const { mutateAsync: getdocumentsByActs, isLoading: docsByActsLoader } = useMutation(documentsByActs);
+  const { mutateAsync: getPreprintedList, isLoading: statusLoader } = useMutation(preprintedList);
+  const { mutateAsync: fusionDocuments, isLoading: fusionLoader } = useMutation(fusionDocumentsWithEsignTreatment);
 
-  const {
-    mutate: getUser,
-    isLoading,
-    isError,
-    
-  } = useMutation(
-    async () => {
-      return await getUserInfos({
-        idRacf: axa_uid_racf,
-        email,
-        name,
-        idRdu: axa_uid_rdu,
-        axaType: axa_type,
+  const { mutateAsync: getSubscribers, isLoading: subscribersLoader } = useMutation(subscribers, {
+    onSuccess: async (data) =>
+      data?.successData?.subscribers
+        ?.split(";")
+        .filter((el: any, index: number) => data?.successData?.subscribers.indexOf(el) !== index)
+        .map((el: any) => addLeadingZeros(el.toString(), 10))
+        .join(";"),
+  });
+  const { mutateAsync: getGed, isLoading: channelLoader } = useMutation(ged);
+
+  /** Function to do esign docs
+   *  Note : This algorithm exists as a activity diagram in docs folders
+   */
+  const startEsignAsync = async () => {
+    let { acts, docs, axapac } = data;
+
+    // if Act view
+    if (acts) {
+      if (!allActsAreEsign(acts)) {
+        await showDematNotEsignModal({ data: getActsNotEsign(acts), modeAct: true });
+        resolverCancel();
+        return;
+      }
+
+      const docsFromActs = await getdocumentsByActs({
+        docFilters,
+        actsIds: acts?.map((act: any) => act.id),
       });
-    },
-    {
-      onSuccess({ successData, authToken }) {
-        setUserAllowed(true);
-        setUser({
-          ...successData
-        });
 
-        if (!filtersInUrl) {
-          delete successData.defaultRequest.pagination;
-          setFilters(successData.defaultRequest);
-        }
-      },
+      docs = getDocsEsign(docsFromActs);
     }
-  );
+
+    // If no esign doc, show message and end
+    if (!docs || !docs.length || !allDocsAreEsign(docs)) {
+      await showDematNotEsignModal({ data: getDocsNotEsign(docs || []), modeAct: false });
+      resolverCancel();
+      return;
+    }
+
+    // check if all docs are same contract number or same client number and same subscriber number
+    if (
+      !allDocsHaveSameContractNumber(docs) ||
+      !allDocsHaveSameClientNumber(docs) ||
+      !allDocsHaveSameSubscriberNumber(docs)
+    ) {
+      await showModalNotSameNumber({ documents: docs });
+      resolverCancel();
+      return;
+    }
+
+    const documentsIds = getDocumentsIds(docs);
+
+    // if demat profile and alls docs are not IL101 and some docs are demat then show demat modal and demat them
+    if (!axapac && !allDocAreIL101(docs) && profile.demat) {
+      const docsToDemat = getDocsToDemat(docs);
+      if (docsToDemat.length > 0) {
+        if (allDocsHaveSameContractNumber(docsToDemat) || allDocsHaveSameClientNumber(docsToDemat)) {
+          const dematDocName =
+            docsToDemat[0].documentOptions.typeLetter === "DEVIS_PROJET_ETUDE" ? "Devis" : "Conditions Particulières";
+          const result = await showDematModal(
+            {
+              document: docs[0],
+            },
+            {
+              title: `Avant de signer électroniquement`,
+              step2Title: `Envoi des ${dematDocName}`,
+              step2Content: (
+                <div>
+                  <h3 className="af-header__title">
+                    <i className="glyphicon glyphicon-file" /> {dematDocName}
+                  </h3>
+                  <p>en cours d'impression</p>
+                  <Loader mode="get" text=" " classModifier="static">
+                    <></>
+                  </Loader>
+                </div>
+              ),
+            }
+          );
+          if (!result.success) {
+            resolverCancel();
+            return;
+          }
+        } else {
+          await showModalNotSameNumber({ documents: docs });
+          resolverCancel();
+          return;
+        }
+      }
+    }
+
+    // get preprints before sending to esign
+    const preprintedsList = await getPreprintedList(documentsIds);
+    let preprintedId: string;
+    if(preprintedsList.length === 0){
+      preprintedId= ''
+    }else{ 
+    if (preprintedsList.length !== 1) {
+      const preprintModalResult = await showModalPrePrint({ preprinteds: preprintedsList });
+      if (!preprintModalResult.success) {
+        resolverCancel();
+        return;
+      }
+      preprintedId = preprintModalResult.preprinted;
+    } else {
+      preprintedId = preprintedsList[0].id;
+    }
+   }
+    // if (preprintedId) {
+    await fusionDocuments({
+      idDocumentList: documentsIds,
+      idPreprinted: preprintedId as string,
+    });
+    // }
+
+    try {
+      await debranchementEsign(docs);
+    } catch (e) {
+      alertError("Une erreur est survenue lors de la génération de l'url de e-signature");
+      resolverCancel();
+    }
+    await updateEsignDocs(documentsIds);
+  };
+
+  const debranchementEsign = async (docs: IDocument[]) => {
+    const documentsIds = getDocumentsIds(docs);
+
+    try {
+      const [subscribers, ged] = await Promise.all([await getSubscribers(documentsIds), await getGed(documentsIds)]);
+      const docsGed = mergeGedWithDocs(docs, ged.successData);
+
+      const esignObject = {
+        identifiantAbonne: subscribers?.successData || [],
+        urlEsign: config ? getDomainForUrl(config.urls.esign, config) : "",
+        ged: docsGed,
+        token: docsGed[0]?.token,
+      };
+
+      let url: any = getEsignUrl(docs, esignObject, config);
+      window.open(url, "_blank");
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const startEsign = async () => {
+    await startEsignAsync();
+  };
+
+  const { mutateAsync: updateEsignDocs, isLoading: updateLoader } = useMutation(updateEsignDocSent, {
+    onSuccess: () => {
+      resolverSuccess();
+    },
+    onError: () => {
+      resolverCancel();
+    },
+  });
 
   useEffect(() => {
-    (async () => {
-      try {
-        if (!name || !accessToken) return;
-        if (accessToken) {
-          setAuthToken(accessToken);
-          setApiToken(accessToken);
-        }
-        await getUser();
-      } catch (error) {
-        console.error("Error while sending user infos to api", error);
+    if (data?.docs || data?.acts) startEsign();
+  }, [data?.docs, data?.acts]);
+
+  return (
+    <Loader
+      mode={
+        subscribersLoader || fusionLoader || statusLoader || channelLoader || updateLoader || docsByActsLoader
+          ? "get"
+          : "none"
       }
-    })();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [email, axa_uid_racf, name, axa_type, accessToken]);
-  return (
-    <>
-      {isLoading ? (
-        <div className="bigCenteredMessage">Chargement de Spoolnet en cours</div>
-      ) : !userAllowed || isError ? (
-        <div className="bigCenteredMessage">
-          Vous n'avez pas accès à Spoolnet, veuillez contacter votre administrateur.
-        </div>
-      ) : (
-        children
-      )}
-    </>
+      text="Chargement en cours"
+    >
+      {" "}
+    </Loader>
   );
 };
 
-export default SendUserInfos;
-import React, { FC, useEffect } from "react";
-import { OidcProvider, OidcSecure, useOidc, useOidcAccessToken } from "@axa-fr/react-oidc";
-import { OIDC_CONFIGURATION } from "@/config/config-env";
-import SendUserInfos from "./SendUserInfos";
+export default EsignResolver;
 
-type Props = {
-  children?: React.ReactNode;
-};
-
-const Oidc: FC<Props> = ({ children }) => {
-
-/*   if (document.location.search.includes("error=access_denied")) {
-
-    return (
-      <div className="bigCenteredMessage">
-        Vous n'avez pas accès à Spoolnet, veuillez contacter votre administrateur.
-      </div>
-    );
-  } */
-  console.log('yy', OIDC_CONFIGURATION);
-  return (
-    <OidcProvider configuration={OIDC_CONFIGURATION}>
-      <OidcSecure>
-        <SendUserInfos>{children}</SendUserInfos>
-      </OidcSecure>
-    </OidcProvider>
-  );
-};
-
-export default Oidc;
-/**
- *  This class decorate oidc hooks from react-oidc and return mocked hooks if oidc is disabled
- *  Use only in development mode
- */
-export { useOidc, useOidcAccessToken, useOidcUser, OidcSecure } from "@axa-fr/react-oidc";
-
-import { BrowserRouter } from "react-router-dom";
-import { QueryClientProvider, QueryClient } from "react-query";
-import "./App.scss";
-import "./theme.scss";
-import ContainerApp from "@/pages/Container/containerApp";
-// import configEnv from "@/config/config-env";
-import AppWithProviders from "./Providers";
-import ModalContainer from "react-modal-promise";
-
-// !Mocking api calls in development mode
-// if (configEnv.useMSW) {
-//   const { worker } = await import("./mocks/mswWorker");
-//   await worker.start({
-//     onUnhandledRequest: "bypass",
-//   });
-// }
-
-const App = () => {
-  const queryClient = new QueryClient();
-  return (
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <AppWithProviders>
-          <ContainerApp />
-          <ModalContainer />
-        </AppWithProviders>
-      </QueryClientProvider>
-    </BrowserRouter>
-  );
-};
-
-export default App;
-import { useChainProviders } from "react-flat-providers";
-import Oidc from "./oidc/Oidc";
-import GlobalAlertProvider from "./components/GlobalAlert/GlobalAlertContext";
-/** provide all the providers and flatten them. */
-import { ReactNode } from "react";
-
-
-type AppWithProvidersProps = {
-  children: ReactNode;
-};
-
-const AppWithProviders = ({ children }: AppWithProvidersProps) => {
-  const FlatChainedProviders = useChainProviders().add(GlobalAlertProvider).add(Oidc).make();
-/*   const a = useOidc();
-  console.log(a);
-  console.log('FlatChainedProviders', FlatChainedProviders); */
- // return <p> bienvenue</p>
- return  <FlatChainedProviders>{children}</FlatChainedProviders>;
-};
-
-export default AppWithProviders;
-import { TokenAutomaticRenewMode, TokenRenewMode } from "@axa-fr/react-oidc";
-import devConfig from "./env-dev.json";
-import stageConfig from "./env.json";
-
-const configEnv: any =
-  import.meta.env.MODE === "development" || import.meta.env.MODE === "test" ? devConfig : stageConfig;
-configEnv.api_url = window.CY_API_URL || import.meta.env.VITE_API_URL || "/api/v1/";
-configEnv.api_url = configEnv.api_url.replace(/\/+$/, "");
-
-configEnv.useMSW = import.meta.env.VITE_API_MOCK && !window.CY_API_URL;
-
-/* OIDC */
-export const OIDC_MOCK_ENABLED = import.meta.env.VITE_OIDC_MOCK_ENABLED === "true";
-
-const OIDC_SERVER_CONFIG = configEnv.OIDC_CONFIGURATION;
-
-OIDC_SERVER_CONFIG.redirect_uri = window.location.origin + OIDC_SERVER_CONFIG.redirect_uri;
-OIDC_SERVER_CONFIG.token_renew_mode = TokenRenewMode.access_token_invalid;
-OIDC_SERVER_CONFIG.token_automatic_renew_mode = TokenAutomaticRenewMode.AutomaticOnlyWhenFetchExecuted;
-
-if (import.meta.env.VITE_LOCAL_ENV) {
-  OIDC_SERVER_CONFIG.authority = "/maam/v2";
-}
-
-export const OIDC_CONFIGURATION = OIDC_SERVER_CONFIG;
-export default configEnv;
-{
-  "api_url": "http://localhost:3000",
-  "OIDC_CONFIGURATION": {
-    "client_id": "9fe8b203",
-    "redirect_uri": "/authentication/callback",
-    "scope": "email openid profile urn:axa:france:SpoolnetNG urn:axa:france:distribution urn:axa:france:tpag urn:axa:france:wac",
-    "authority": "https://onelogin.dev.axa.com",
-    "service_worker_only": false
-  }
-}
-
-lorsque je le correct avec quelqun inconnu  jai ca dans url et  qui se boucle dan infini http://localhost:3000/authentication/callback?error_description=Not+authorized+-+User+not+known&iss=https%3A%2F%2Fonelogin.dev.axa.com&state=BB3TRzf3tNhDC0Cl&error=access_denied#.  mais moi je veux que se sois message comme si vous navze pas acces a spoolnet  car je nai pas amimer la facon comme ca 
-  if (document.location.search.includes("error=access_denied")) {
-
-    return (
-      <div className="bigCenteredMessage">
-        Vous n'avez pas accès à Spoolnet, veuillez contacter votre administrateur.
-      </div>
-    );
-  
+dans ce code jai fai ca    if(preprintedsList.length === 0){
+      preprintedId= ''
+    } pour ne ma afficher showmidal  est ce que cest bien le if et le else 
