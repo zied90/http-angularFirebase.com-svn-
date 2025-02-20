d941c6a139f63c28bc96b6bd43db97742a3b1a3b
zied <zied.elmiladi@gmail.com>
2025-02-20 19:18:05 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,13 @@
  search: `?type=${state.search.editionType}&name=${state.search.editionName}&receptionDate=${state.search.receptionDate}&fluxStatus=${state.search.fluxStatus}&status=${status}&fluxSubmit=${state.search.fluxSubmit}`,
 
 
-    search: `workspaceId=${workspaceId}&type=${searchParmsDoc.type}&name=${searchParmsDoc.name}&sortField=receptionDate&sortOrder=desc&page=0&size=10&fluxStatus=${searchParmsDoc.fluxStatus}&fluxSubmit=${searchParmsDoc.fluxSubmit}&receptionDate=${searchParmsDoc.receptionDate}`
+      <Link
+                        className="btn af-btn af-btn--quote"
+                                to={{
+                                    pathname: `/workspaces/${workspaceId}/flow-list`,
+                                    search: `workspaceId=${workspaceId}&type=${searchParmsDoc.type}&name=${searchParmsDoc.name}&sortField=receptionDate&sortOrder=desc&page=0&size=10&fluxStatus=${searchParmsDoc.fluxStatus}&fluxSubmit=${searchParmsDoc.fluxSubmit}&receptionDate=${searchParmsDoc.receptionDate}`
+                                }}
+                                >
+                            <i className="glyphicon glyphicon-arrowthin-left"/>
+                            <span className="af-btn__text" style={{paddingLeft: "2rem"}}>Retour à la liste des fluxxxxxxxx </span>
+                        </Link>
