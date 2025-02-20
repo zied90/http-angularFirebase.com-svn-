5812da8292ef45fc8d1b9a07583e8afdffa57d77
zied <zied.elmiladi@gmail.com>
2025-02-20 19:12:07 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,4 @@
  search: `?type=${state.search.editionType}&name=${state.search.editionName}&receptionDate=${state.search.receptionDate}&fluxStatus=${state.search.fluxStatus}&status=${status}&fluxSubmit=${state.search.fluxSubmit}`,
+
+
+    search: `workspaceId=${workspaceId}&type=${searchParmsDoc.type}&name=${searchParmsDoc.name}&sortField=receptionDate&sortOrder=desc&page=0&size=10&fluxStatus=${searchParmsDoc.fluxStatus}&fluxSubmit=${searchParmsDoc.fluxSubmit}&receptionDate=${searchParmsDoc.receptionDate}`
