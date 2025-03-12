f5c35e807ba76c1f43421f6006e08fb9af5894ca
zied <zied.elmiladi@gmail.com>
2025-03-12 10:33:34 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,226 +1,138 @@
-import React, {FunctionComponent, useEffect, useReducer, useState} from "react";
-import Table from "@axa-fr/react-toolkit-table";
-import ErrorMessage from "models/errorMessage";
-import errorIcon from "assets/svg/error.svg";
-import {fluxDurationReducer} from "components/admin/flux-duration/flux-duration-reducer";
-import Title from "@axa-fr/react-toolkit-title";
+import React, { FunctionComponent, useEffect, useState } from "react";
 // @ts-ignore
-import {Link, RouteComponentProps} from "react-router-dom";
+import { NavBar, NavBarItem, Title } from "@axa-fr/react-toolkit-layout-header";
 // @ts-ignore
-import Loader from "@axa-fr/react-toolkit-loader";
-import './flux-duration.scss';
-import userInfo from "models/userInfo";
-import FluxDurationService from "../../../api/flux-duration.service";
-import {compose} from "recompose";
-import {withAuthentication} from "@axa-fr/react-oidc-context-fetch";
-import Toast from "../../toast";
+import "./nav-bar.scss";
+import UserInfo from "models/userInfo";
+import { useLocation } from "react-router-dom";
+import UserService, { AuthoritiesEnum } from "api/user.service";
+import { Link } from "react-router-dom";
 
-
-export interface IFetch {
-    fetch?: (input: RequestInfo, init?: RequestInit) => Promise<Response>;
+interface Usr {
+  usr: UserInfo;
+  title: string;
+  fetch?: (input: RequestInfo, init?: RequestInit) => Promise<Response>;
 }
 
-interface Props {
-    fetch?: (input: RequestInfo, init?: RequestInit) => Promise<Response>,
-    usr: userInfo
-}
-
-type Params = {}
-
-const FluxDuration: FunctionComponent<Props> = ({usr, fetch}) => {
-
-    const [errorMessage, setErrorMessage] = useState(new ErrorMessage(false, "", "", errorIcon));
-
-    const [state, dispatch] = useReducer(fluxDurationReducer, {
-        loaderMode: "none",
-        fluxDurations: []
-    });
-
-    useEffect(() => {
-            let isCancelled = false;
-            if (!isCancelled)
-                getFluxDurations();
-            return () => {
-                isCancelled = true;
-            };
-        }, []
-    );
-
-    const getFluxDurations = () => {
-        dispatch({type: "ON_LOAD", loaderMode: "get"});
-        FluxDurationService.getFluxDurations(typeof (fetch) === 'undefined' ? window.fetch : fetch).then(f => {
-            dispatch({
-                type: "ON_LOAD_FLUX_DURATION",
-                fluxDurations: f
-            });
-            dispatch({type: "ON_LOAD", loaderMode: "none"});
-        }).catch(error => {
-                dispatch({
-                    type: "ON_LOAD_FLUX_DURATION",
-                    fluxDurations: []
-                });
-                dispatch({type: "ON_LOAD", loaderMode: "none"});
-                let message = JSON.parse(error.message);
-                setErrorMessage(new ErrorMessage(true, message.message.substring(0, 300) + "...", "Recherche des durées", errorIcon));
-            }
-        )
-
-    };
-
-    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-
-        const key = e.key;
-        if (key === 'Enter') {
-            e.preventDefault();
-        }
-    };
-
-    const isNumeric = (s: string): boolean => {
-        return !isNaN(+s);
+const NavBarApp: FunctionComponent<Usr> = (usr) => {
+  const location = useLocation();
+  const [visible, setVisible] = useState<boolean>(false);
+  const [itemPosition, setItemPosition] = useState<number | null | undefined>(
+    0
+  );
+  const handleClickItem = (position: number) => {
+    setItemPosition(position);
+  };
+  const handleClick = () => {
+    const { body } = document;
+    body.classList.toggle("af-menu-open");
+    const visibleTmp = !visible;
+    setVisible(visibleTmp);
+  };
+  useEffect(() => {
+    const path: string = location.pathname;
+    const pathsConfiguration = [
+      "/users",
+      "/notifications",
+      "/projects",
+      "UserManagement",
+      "projectCreation",
+      "/workspaces",
+    ];
+    if (path && pathsConfiguration.some((v) => path.includes(v))) {
+      setItemPosition(1);
     }
+  }, [itemPosition]);
+  return (
+    <>
+      <NavBar
+        isVisible={false}
+        positionInit={itemPosition}
+        onClick={handleClick}
+      >
+        <NavBarItem
+        key="doc-1"
+          arial-label="menuItem1"
+          actionElt={
+            <Link
+              className="af-nav__link"
+              to={"/"}
+              onClick={() => handleClickItem(0)}
+            >
+              {" "}
+              Gestion des flux
+            </Link>
+          }
+          classModifier={itemPosition === 0 ? "active" : ""}
+        />
 
-    const handleChangeText = (sensitive: boolean, input: string, e: React.ChangeEvent<HTMLInputElement>) => {
-        const value = e.target.value;
-        fluxDurations.filter(f => f.sensitiveData === sensitive).map(flux => {
-            if (input === "validation" && isNumeric(value)) {
-                flux.validationInvalidationDelay = +value;
-                dispatch({type: "ON_UPDATE_FLUX_DURATION", fluxDurations: fluxDurations})
-            }
+        {UserService.hasPrivilege(
+          usr.usr.authorityList,
+          AuthoritiesEnum.ADMINISTRATEUR
+        ) ? (
+          <NavBarItem
+            key="doc-2"
+            arial-label="menuItem2"
+            actionElt={<span className="af-nav__link">Configuration</span>}
+            classModifier={itemPosition === 1 ? "active" : ""}
+          >
+            <NavBarItem
+              key="workspaces"
+              actionElt={
+                <Link
+                  className="af-nav__link two"
+                  to={"/workspaces"}
+                  onClick={() => handleClickItem(1)}
+                >
+                  Workspaces
+                </Link>
+              }
+            />
+            <NavBarItem
+            key="table-1"
+              actionElt={
+                <Link
+                  className="af-nav__link two"
+                  to={"/flux-duration"}
+                  onClick={() => handleClickItem(1)}
+                >
+                  Configuration des durées maximales
+                </Link>
+              }
+            />
+            <NavBarItem
+             key="table-2"
+              actionElt={
+                <Link
+                  to={"/projects"}
+                  className="af-nav__link two"
+                  onClick={() => handleClickItem(1)}
+                >
+                  Projets
+                </Link>
+              }
+              classModifier={itemPosition === 1 ? "active" : ""}
+            />
+            <NavBarItem
+            key="table-1"
+              actionElt={
+                <Link
+                  className="af-nav__link two"
+                  to={"/users"}
+                  onClick={() => handleClickItem(1)}
+                >
+                  Utilisateurs
+                </Link>
+              }
+              classModifier={itemPosition === 1 ? "active" : ""}
+            />
+          </NavBarItem>
+        ) : null}
+      </NavBar>
+      <Title title={usr.title} toggleMenu={handleClick} />
+    </>
+  );
+};
 
-            if (input === "submission" && isNumeric(value)) {
-                flux.submissionDelay = +value;
-                dispatch({type: "ON_UPDATE_FLUX_DURATION", fluxDurations: fluxDurations})
-            }
-
-            if (input === "retention" && isNumeric(value)) {
-                flux.retentionPeriod = +value;
-                dispatch({type: "ON_UPDATE_FLUX_DURATION", fluxDurations: fluxDurations})
-
-            }
-        })
-
-
-
-    }
-
-    const saveData = (e: any) => {
-        dispatch({type: "ON_LOAD", loaderMode: "get"});
-        FluxDurationService.saveFluxDuration(typeof (fetch) === 'undefined' ? window.fetch : fetch, fluxDurations).then(res => {
-            dispatch({type: "ON_LOAD", loaderMode: "none"});
-        }).catch(error => {
-                dispatch({
-                    type: "ON_LOAD_FLUX_DURATION",
-                    fluxDurations: fluxDurations
-                });
-
-                let message = JSON.parse(error.message);
-                setErrorMessage(new ErrorMessage(true, message.message.substring(0, 300) + "...", "Recherche des durées", errorIcon));
-            }
-        )
-    }
-
-    const {
-        loaderMode,
-        fluxDurations
-    } = state;
-
-
-    return (
-        <Loader mode={loaderMode}>
-            <>
-                {
-                    errorMessage.hasErrors ?
-                        <Toast
-                            errorMessage={errorMessage}
-                            position={'bottom-right'}
-                            autoDelete={true}
-                            dismissTime={10000}
-                        />
-                        : ""
-                }
-
-
-                <div style={{marginBottom: "2rem", marginTop: "2rem"}}>
-                    <Title>Gestion de la configuration des durées</Title>
-                </div>
-
-                <Table className="af-table one">
-                    <Table.Header>
-                        <Table.Tr>
-                            <Table.Th>
-                                <span className="af-table__th-content">Sensible</span>
-                            </Table.Th>
-
-                            <Table.Th>
-                                <span className="af-table__th-content">Validation / Invalidation auto max</span>
-                            </Table.Th>
-
-                            <Table.Th>
-                                <span className="af-table__th-content">Soumission auto max</span>
-                            </Table.Th>
-
-                            <Table.Th>
-                                <span className="af-table__th-content">Durée de conservation des flux traités</span>
-                            </Table.Th>
-                        </Table.Tr>
-                    </Table.Header>
-                    <Table.Body>
-                        {
-                            fluxDurations && fluxDurations.map(({
-                                                                    sensitiveData,
-                                                                    validationInvalidationDelay,
-                                                                    submissionDelay,
-                                                                    retentionPeriod
-                                                                }, i) => (
-                                <Table.Tr>
-                                    <Table.Td>
-                                        <span className="af-table__th-content">{sensitiveData ? "Oui" : "Non"}</span>
-                                    </Table.Td>
-                                    <Table.Td>
-                                        <input type="number" className="form-control one"
-                                               placeholder="400" defaultValue={validationInvalidationDelay}
-                                               onKeyDown={(e) => handleKeyDown(e)}
-                                               onChange={(e) => handleChangeText(sensitiveData, 'validation', e)}
-                                        />
-                                    </Table.Td>
-                                    <Table.Td>
-                                        <input type="number" className="form-control one"
-                                               placeholder="400" defaultValue={submissionDelay}
-                                               onKeyDown={(e) => handleKeyDown(e)}
-                                               onChange={(e) => handleChangeText(sensitiveData, 'submission', e)}
-                                        />
-                                    </Table.Td>
-                                    <Table.Td>
-                                        <input type="number" className="form-control one"
-                                               placeholder="400" defaultValue={retentionPeriod}
-                                               onKeyDown={(e) => handleKeyDown(e)}
-                                               onChange={(e) => handleChangeText(sensitiveData, 'retention', e)}
-                                        />
-                                    </Table.Td>
-                                </Table.Tr>
-                            ))
-                        }
-
-                    </Table.Body>
-                </Table>
-                <div className={"row"}>
-                    <div className="col-md-12">
-                        <button className={"afn-af-btn af-btn float-right"}
-                                title="Sauvegarder les données"
-                                onClick={e => saveData(e)}>Sauvegarder les données
-                        </button>
-                    </div>
-                </div>
-            </>
-        </Loader>
-
-    );
-
-}
-const enhanceAuth = compose<Props, Props>(
-    withAuthentication(fetch)
-);
-
-const FluxDurationEnhance = enhanceAuth(FluxDuration);
-
-export default FluxDurationEnhance;
+export default NavBarApp;
+ et eci mais nul si la constion nest pas valide etr que ne sera afficher
