f6595e095dc2f70e2ad7d8d956da0a5aea547323
zied <zied.elmiladi@gmail.com>
2025-03-13 11:46:17 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,1 @@
-Expected: "Déjà Formaté"
-Received: "DÉJÀ FormatÉ"
-
- ❯ src/Admin/adminUtils/FormatString.test.ts:8:52
-      6|   });
-      7|   it("ne modifie pas un texte déjà formaté", () => {
-      8|     expect(formatStringForDisplay("Déjà Formaté")).toBe("Déjà Formaté");
-       |                                                    ^
-      9|   });
-     10|   it("gère les chaînes vides", () => {
+af-btn--circle af-btn--circle-reverse   ca aprtioen a quel bibliotheque 
