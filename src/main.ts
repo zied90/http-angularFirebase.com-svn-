aa67c463dbeef81c3e93c00aa4fd0b3629317133
zied <zied.elmiladi@gmail.com>
2025-03-26 14:42:03 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,1 @@
- concerenat les ticket spoolnet v3
-https://ajir.axa-fr.intraxa/browse/DEMAT-16353  deja fait 
 
-
-
-https://ajir.axa-fr.intraxa/browse/DEMAT-16215  => Irritants switch affichage dossier ca dapres ce que jai compris depuis noitre derniere point ce que Arnaud  a dit qui  va traiter ca vu que  puisque il a deja le code dans  un autre projet 
-
-https://ajir.axa-fr.intraxa/browse/DEMAT-16329  =>Alignement Entête Spoolnet cest fait
-https://ajir.axa-fr.intraxa/browse/DEMAT-16251  => Correction "Impression"  cest fait 
-
-https://ajir.axa-fr.intraxa/browse/DEMAT-16269  => Nommage Enregistrer / Mail / Onglet coté backend a faire
-
-https://ajir.axa-fr.intraxa/browse/DEMAT-16395=>Formatage de texte "Aucune action réalisé pour ce tickt voici une captured'ecran ou si tu veux je peux te montrer 
