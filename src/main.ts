a91120d74923f01b31608fdaee806c8747649361
zied <zied.elmiladi@gmail.com>
2025-04-15 11:09:16 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,9 @@
-     × should set the correct value to localStorage
-
-⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯ Failed Tests 1 ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯
- FAIL  src/utils/localStorage.test.ts > LocalStorage utilities > should set the correct value to localStorage
-AssertionError: expected "setItem" to be called with arguments: [ 'groupByAct', 'true' ]
-
-Received:
-
-
-
-Number of calls: 0
-
- ❯ src/utils/localStorage.test.ts:90:17
-     88|     setGroupByActInLocalStorage(true);
-     90|     expect(spy).toHaveBeenCalledWith("groupByAct", "true");
-       |                 ^
-     91|   });
-     92| });
+ src/utils/localStorage.test.ts > LocalStorage utilities > should set the correct value to localStorage
+Error: setItem does not exist
+ ❯ src/utils/localStorage.test.ts:85:20
+     83|   it("should set the correct value to localStorage", () => {
+     84|     // Espionner directement global.localStorage.setItem
+     85|     const spy = vi.spyOn(global.localStorage, "setItem");
+       |                    ^
+     86|     setGroupByActInLocalStorage(true);
+     87|     // Vérifier que setItem a bien été appelé avec les bons arguments
