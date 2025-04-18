f40bb20d63e5df84b0ab4f9d21fbb9b8b6b9ae65
zied <zied.elmiladi@gmail.com>
2025-04-18 17:46:07 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
     @Override
     public ResponseEntity<byte[]> show() {
-
         var userDetail = userService.getUserDetailsCustom();
         Jwt token = userDetail.getToken();
 
@@ -12,12 +11,25 @@
         }
 
         try {
+
+            HttpHeaders httpHeaders = new HttpHeaders();
+
             byte[] binary = documentServicesManager.documentsService()
                 .getDocumentFile(id.longValue());
-            HttpHeaders httpHeaders = new HttpHeaders();
+
+            documentServicesManager.documentsService()
+                .findOneById(id.longValue()).ifPresentOrElse(d -> httpHeaders.setContentDisposition(
+                    ContentDisposition.inline()
+                        .filename("yyyyyyy" + ".pdf")
+                        .build()
+                ), () -> httpHeaders.setContentDisposition(
+                    ContentDisposition.attachment()
+                        .filename( "rrrrrrr.pdf")
+                        .build()
+                ));
+
+
             httpHeaders.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_PDF_VALUE);
-            httpHeaders.add(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=a.pdf");
-            System.out.println("httpHeaders"+httpHeaders);
             return ResponseAPI.good(binary, httpHeaders).toResponseEntity();
         } catch (IOException e) {
             throw new DocumentNotFoundException("Document " + id + " not found");
