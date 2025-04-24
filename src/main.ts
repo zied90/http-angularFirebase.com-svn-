31e3040afff6a2fcc10fa12750188782d9e421a4
zied <zied.elmiladi@gmail.com>
2025-04-24 11:28:39 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,1 @@
-    public ExportDocumentResponse exportDocument(ExportDocument exportDocument)
-        throws Exception {
-        LOGGER.info("Start export with id request {}",
-            exportDocument.getExportDocument().getIdRequest());
 
-        fr.axa.pfel.wspfelv3.domain.document.ExportDocument exportDocumentDTO = exportDocumentMapper.toDto(
-            exportDocument.getExportDocument());
-        final var attachmentDomain = exportDocumentService.exportDocument(exportDocumentDTO);
-        final var exportDocumentResponse = new ExportDocumentResponse();
-        exportDocumentResponse.setAttachment(
-            attachmentResponseMapper.attDomainToAttResponse(attachmentDomain));
-
-        return exportDocumentResponse;
-
-
-    }
-  estce que cette fonction a chaque fois quon lappel elle retourne excption ?
