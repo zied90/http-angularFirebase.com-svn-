b5b292f492bfcff2710cf6872856d75b977566a4
zied <zied.elmiladi@gmail.com>
2025-04-29 10:01:38 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
 
+    /**
+     * Call SearchDocuments from GED to retrieve IDs from corresponding documents
+     */
+    @Override
+    public SearchDocumentsMetadatasResponse searchDocumentsFromGed(String user, String ecmRefDoc,
+                                                                   String... projects) {
+        SearchDocumentsMetadatasResponse SearchDocumentsMetadatasResponse = null;
+        SearchDocumentsMetadatasRequest documentsMetadatasRequest=new SearchDocumentsMetadatasRequest();
+        CommonParameters commonParameters= new CommonParameters();
+        commonParameters.setLocale(CommonParameters.LocaleEnum.FR_FR);
+        commonParameters.endUserType(user);
+        documentsMetadatasRequest.setCommonParameters(commonParameters);
+        documentsMetadatasRequest.setPredicate(Constants.PREDICATE_STRING + "('" + ecmRefDoc + "')");
+        SearchDocumentsMetadatasResponse=  defaultApi.documentMetadatasSearchPost(documentsMetadatasRequest);
+        return SearchDocumentsMetadatasResponse;
+    }
