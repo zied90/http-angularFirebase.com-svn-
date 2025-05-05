55554179a3c086abb6b96aec2c835c038813b43c
zied <zied.elmiladi@gmail.com>
2025-05-05 13:37:09 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,177 +1,28 @@
-clients:
-  fr-ged-api-documents-v2-vs:
-    endpoint: https://eip-rec.axa-fr.intraxa/ws/fr-ged-api-documents-v2-vs
-    compress: false
-    auth: oauth
-    auth-config:
-      grant-type: application
-      token-endpoint: https://onelogin.dev.axa.com/as/token.oauth2
-      scopes: urn:axa:france:ged:cst urn:axa:france:ged:mng urn:axa:france:ged:src
-      client-id: 51388898
-      client-secret: exempetscrettttt
-      client-assertion: jwt
+--NMCY-4TjzCTbfUNDMh4YgXTAkXHfMd
+Content-Disposition: form-data; name="file"
+Content-Type: application/octet-stream
+Content-Length: 56194
 
+GIF89a��3f
+>���}�,�Q�rgkZ�p�<��}�Z\Ag0�
+��T��N�˥�Q�`�7�bEKi��c����W܊��ε.\)�W�l���m��3c󎤰5�zWtL׼�?���!;
+--NMCY-4TjzCTbfUNDMh4YgXTAkXHfMd
+Content-Disposition: form-data; name="getDocumentContentResponse"
+Content-Type: text/plain;charset=UTF-8
+Content-Length: 71
 
-    @Bean
-    public GetDocumentApi frGetApiDocumentsV2Vs(RestClientBuilderFactory restClientBuilderFactory){
-        return restClientBuilderFactory
-                .configureFromEnvironment(GED_REST_SERVICE)
-                //.withInterceptors(template->template.header("Accept", ""))
-                //.withCodec(new Encoder.Default(), new Decoder.Default())
-                .createClient(GetDocumentApi.class);
-    }
-
-
-openapi: 3.0.0
-info:
-  version: "1.0"
-  title: "GED - GetDocumentContent"
-paths:
-  /document/binary/content:
-    post:
-      tags:
-        - getDocument
-      summary: "GetDocumentContent / multi part response"
-      requestBody:
-        description: "GetDocumentContent Request"
-        content:
-          application/json:
-            schema:
-              $ref: "#/components/schemas/GetDocumentContentRequest"
-        required: true
-      responses:
-        "200":
-          description: "OK"
-          content:
-            multipart/form-data: # <-- Content-Type of the response
-              schema:
-                type: object
-                properties:
-                  getDocumentContentResponse:
-                    $ref: '#/components/schemas/GetDocumentContentResponse'
-                  file:
-                    type: string
-                    format: binary
-        "500":
-          description: "KO"
-          content:
-            application/json:
-              schema:
-                $ref: "#/components/schemas/BusinessError"
-      security:
-        - OAuth2:
-            - urn:axa:france:ged:cst
-components:
-  securitySchemes:
-    OAuth2:
-      type: oauth2
-      flows:
-        clientCredentials:
-          tokenUrl: https://maam-dev.axa.com/maam/v2/token
-          scopes:
-            urn:axa:france:ged:cst: Consult
-  schemas:
-    BusinessError:
-      type: object
-      properties:
-        Status:
-          type: string
-        Code:
-          type: string
-        Label:
-          type: string
-        Message:
-          type: string
-    GetDocumentContentRequest:
-      type: object
-      properties:
-        CommonParameters:
-          $ref: "#/components/schemas/CommonParameters"
-        DocId:
-          type: string
-      required:
-        - DocId
-        - CommonParameters
-    GetDocumentContentResponse:
-      type: object
-      properties:
-        Status:
-          type: string
-        Name:
-          type: string
-        Extension:
-          type: string
-        MimeType:
-          type: string
-
-    CommonParameters:
-      type: object
-      properties:
-        Locale:
-          type: string
-          enum: [fr-fr, en-us]
-        EndUserId:
-          type: string
-        EndUserType:
-          type: string
-        ApplicationCaller:
-          type: string
-        ObjectStore:
-          type: string
-      required:
-        - Locale
-
-
-apartir de swagger ona generer ca  package fr.axa.pfel.client.impl;
-
-import fr.axa.honey.client.rest.feign.ApiClient;
-import fr.axa.honey.client.rest.feign.EncodingUtils;
-import fr.axa.pfel.client.model.ApiResponse;
-
-import fr.axa.pfel.client.model.BusinessError;
-import fr.axa.pfel.client.model.DocumentBinaryContentPost200Response;
-import fr.axa.pfel.client.model.GetDocumentContentRequest;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import feign.*;
-
-@jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2025-05-05T13:08:36.846685100+02:00[Europe/Paris]", comments = "Generator version: 7.9.0")
-public interface GetDocumentApi extends ApiClient.Api {
-
-
-  /**
-   * GetDocumentContent / multi part response
-   * 
-   * @param getDocumentContentRequest GetDocumentContent Request (required)
-   * @return DocumentBinaryContentPost200Response
-   */
-  @RequestLine("POST /document/binary/content")
-  @Headers({
-    "Content-Type: application/json",
-    "Accept: application/json,multipart/form-data",
-  })
-  DocumentBinaryContentPost200Response documentBinaryContentPost(GetDocumentContentRequest getDocumentContentRequest);
-
-  /**
-   * GetDocumentContent / multi part response
-   * Similar to <code>documentBinaryContentPost</code> but it also returns the http response headers .
-   * 
-   * @param getDocumentContentRequest GetDocumentContent Request (required)
-   * @return A ApiResponse that wraps the response boyd and the http headers.
-   */
-  @RequestLine("POST /document/binary/content")
-  @Headers({
-    "Content-Type: application/json",
-    "Accept: application/json,multipart/form-data",
-  })
-  ApiResponse<DocumentBinaryContentPost200Response> documentBinaryContentPostWithHttpInfo(GetDocumentContentRequest getDocumentContentRequest);
-
-
-}
-  avec des model dans target       <dependency>
-      <groupId>fr.axa.honey.framework</groupId>
-      <artifactId>honey-restclient-starter</artifactId>
-    </dependency>
+{"status":"OK","name":"doc10","extension":"gif","mimeType":"image/gif"}
+--NMCY-4TjzCTbfUNDMh4YgXTAkXHfMd--  et dans le hedaer ilya ca date	Mon, 05 May 2025 11:35:43 GMT
+content-type	multipart/form-data;boundary=NMCY-4TjzCTbfUNDMh4YgXTAkXHfMd
+transfer-encoding	chunked
+connection	keep-alive
+expires	0
+vary	Origin,Access-Control-Request-Method,Access-Control-Request-Headers
+axa-client-id	51388898
+x-frame-options	DENY
+pragma	no-cache
+strict-transport-security	max-age=31536000 ; includeSubDomains
+x-content-type-options	nosniff
+x-xss-protection	0
+axa-correlation-id	60d522d4-2a8e-73b5-1718-0000373da751
+cache-control	no-cache, no-store, max-age=0, must-revalidate  cest lappel avec bruno
