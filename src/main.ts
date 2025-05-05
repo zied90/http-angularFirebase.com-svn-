ad97229d145e8ac842c25455e7808c91b7d5420c
zied <zied.elmiladi@gmail.com>
2025-05-05 13:58:44 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,19 @@
---NMCY-4TjzCTbfUNDMh4YgXTAkXHfMd
-Content-Disposition: form-data; name="file"
-Content-Type: application/octet-stream
-Content-Length: 56194
-
-GIF89a��3f
->���}�,�Q�rgkZ�p�<��}�Z\Ag0�
-��T��N�˥�Q�`�7�bEKi��c����W܊��ε.\)�W�l���m��3c󎤰5�zWtL׼�?���!;
---NMCY-4TjzCTbfUNDMh4YgXTAkXHfMd
-Content-Disposition: form-data; name="getDocumentContentResponse"
-Content-Type: text/plain;charset=UTF-8
-Content-Length: 71
-
-{"status":"OK","name":"doc10","extension":"gif","mimeType":"image/gif"}
---NMCY-4TjzCTbfUNDMh4YgXTAkXHfMd--  et dans le hedaer ilya ca date	Mon, 05 May 2025 11:35:43 GMT
-content-type	multipart/form-data;boundary=NMCY-4TjzCTbfUNDMh4YgXTAkXHfMd
-transfer-encoding	chunked
-connection	keep-alive
-expires	0
-vary	Origin,Access-Control-Request-Method,Access-Control-Request-Headers
-axa-client-id	51388898
-x-frame-options	DENY
-pragma	no-cache
-strict-transport-security	max-age=31536000 ; includeSubDomains
-x-content-type-options	nosniff
-x-xss-protection	0
-axa-correlation-id	60d522d4-2a8e-73b5-1718-0000373da751
-cache-control	no-cache, no-store, max-age=0, must-revalidate  cest lappel avec bruno
+Request Body schema: multipart/form-data
+required
+UploadRequest
+required
+object (UploadDocumentRequest)
+CommonParameters
+required
+object (CommonParameters)
+Interaction	
+string (Interaction)
+Enum: "sync" "async"
+CallbackAddress	
+string
+DocumentProperties
+required
+object (DocumentProperties)
+Content
+required
+string <binary>
