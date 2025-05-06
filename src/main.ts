bcafdd640b4a7d58aeabd3543d1948103e9e968c
zied <zied.elmiladi@gmail.com>
2025-05-06 10:32:54 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,12 @@
-CommonParameters:{
-"Locale": "fr-fr",
-"EndUserId": "S875170",
-  "ApplicationCaller": "SOLARIS_UAP"
-},
-DocumentProperties:{
-   "Name": "Contrat_Client",
-   "Extension": "pdf",
- 
-   "Properties": [
-     {
-       "Key": "Author",
-       "Value": "John Doe"
-     },
-     {
-       "Key": "CreationDate",
-       "Value": "2023-10-05"
-     }
-
-
-     estb cez que cest correct jai mit ca dans valeur  de dans Multipar form et le key cest UploadDocumentRequest
+[INFO] 
+[INFO] --- compiler:3.13.0:compile (default-compile) @ ged-service-impl ---
+[INFO] Recompiling the module because of changed source code.
+[INFO] Compiling 8 source files with javac [debug parameters release 21] to target\classes
+[INFO] -------------------------------------------------------------
+[ERROR] COMPILATION ERROR : 
+[INFO] -------------------------------------------------------------
+[ERROR] /C:/dev/Backend/wspfelV3/ged-service-impl/src/main/java/fr/axa/pfel/wspfelv3/ged/impl/service/GedServiceImpl.java:[126,92] cannot find symbol
+  symbol:   variable LocaleEnum
+  location: class fr.axa.pfel.client.model.CommonParameters
+[INFO] 1 error
+[INFO] -------------------------------------------------------------
