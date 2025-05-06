06be5aec0d091eeb298e8efef48e92f0f5350fc8
zied <zied.elmiladi@gmail.com>
2025-05-06 14:23:48 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,1 @@
-[INFO] 
-[INFO] --- compiler:3.13.0:compile (default-compile) @ ged-service-impl ---
-[INFO] Recompiling the module because of changed source code.
-[INFO] Compiling 8 source files with javac [debug parameters release 21] to target\classes
-[INFO] -------------------------------------------------------------
-[ERROR] COMPILATION ERROR : 
-[INFO] -------------------------------------------------------------
-[ERROR] /C:/dev/Backend/wspfelV3/ged-service-impl/src/main/java/fr/axa/pfel/wspfelv3/ged/impl/service/GedServiceImpl.java:[126,92] cannot find symbol
-  symbol:   variable LocaleEnum
-  location: class fr.axa.pfel.client.model.CommonParameters
-[INFO] 1 error
-[INFO] -------------------------------------------------------------
+ @Query("select docLive from DocumentLiveEntity docLive join docLive.doc doc1 where docLive.doc.idRequest = ?1 ")
