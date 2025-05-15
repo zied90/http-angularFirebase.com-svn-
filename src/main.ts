e9bb6aa8b4e83b5d62bde2e7d4107cf3027a68f5
zied <zied.elmiladi@gmail.com>
2025-05-15 11:02:22 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,64 @@
+import { useQuery } from "react-query";
+import Loader from "@axa-fr/react-toolkit-loader";
+import ListActs from "@/components/documents/listActs";
+import filtersStore from "@/stores/documents/filtersStore";
+import documentStore from "@/stores/documents/documentStore";
+import userStore from "@/stores/userStore";
+import { findDocumentsByacts } from "@/api/apifunctions/findDocumentsByAct";
+import useFilters from "../Filters/useFilters";
+import useDocFilters from "@/hooks/useDocFilters";
+import EdiSubcribe from "@/components/EdiSubcribe";
+import { IUser } from "@/types/user";
+import { Alert } from "@axa-fr/react-toolkit-all";
+
+const ActsApp: React.FC = () => {
+  const { openedActs } = filtersStore();
+  const { filters } = useFilters();
+
+  const { docFilters } = useDocFilters();
+
+  const { documentsByAct, setDocumentsByAct } = documentStore();
+  const { token, user } = userStore();
+  const {
+    profile: { subscribeEdi },
+    ediSubscriptionUrl,
+  } = user as IUser;
+
+  const { isError, isFetching } = useQuery(
+    ["findact", docFilters, token],
+    () =>
+      findDocumentsByacts({
+        ...docFilters,
+        actOpened: openedActs?.toString(),
+      }),
+    {
+      onSuccess({ successData }) {
+        setDocumentsByAct(successData);
+      },
+      enabled: !!(filters && token),
+      refetchOnWindowFocus: false,
+      keepPreviousData: true,
+    }
+  );
+
+  return (
+    <>
+      {isError && (
+        <div className="p-2">
+          <Alert classModifier="error" title="Une erreur est survenue lors du chargement des Actes." />
+        </div>
+      )}
+      <Loader mode={isFetching ? "get" : "none"} text="Chargement en cours">
+        {documentsByAct && (
+          <>
+            <EdiSubcribe subscribeEdi={subscribeEdi} subscriptionUrl={ediSubscriptionUrl} />
+            <ListActs />
+          </>
+        )}
+      </Loader>
+    </>
+  );
+};
+
+export default ActsApp;
 
