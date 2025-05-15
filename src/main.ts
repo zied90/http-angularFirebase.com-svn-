f4f42520bb63a775065a6b0a4ea047a53611f3a5
zied <zied.elmiladi@gmail.com>
2025-05-15 14:30:50 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,64 +1,6 @@
-import { useQuery } from "react-query";
-import Loader from "@axa-fr/react-toolkit-loader";
-import ListActs from "@/components/documents/listActs";
-import filtersStore from "@/stores/documents/filtersStore";
-import documentStore from "@/stores/documents/documentStore";
-import userStore from "@/stores/userStore";
-import { findDocumentsByacts } from "@/api/apifunctions/findDocumentsByAct";
-import useFilters from "../Filters/useFilters";
-import useDocFilters from "@/hooks/useDocFilters";
-import EdiSubcribe from "@/components/EdiSubcribe";
-import { IUser } from "@/types/user";
-import { Alert } from "@axa-fr/react-toolkit-all";
+2 point qui  ne sont pas claire dans  upload d’un document :
+ La premiere :
+Ca veut dire quoi : Pour récupérer le MODOP "ASPOSE Outil Upload" cliquez sur village.axa-fr.intraxa 
+Et ce Modop consitre a faire  quoi  et est ce que ce lien est consomable ou non dans l ancienne version et est ce que on va le garder dans la nouvel console 
 
-const ActsApp: React.FC = () => {
-  const { openedActs } = filtersStore();
-  const { filters } = useFilters();
-
-  const { docFilters } = useDocFilters();
-
-  const { documentsByAct, setDocumentsByAct } = documentStore();
-  const { token, user } = userStore();
-  const {
-    profile: { subscribeEdi },
-    ediSubscriptionUrl,
-  } = user as IUser;
-
-  const { isError, isFetching } = useQuery(
-    ["findact", docFilters, token],
-    () =>
-      findDocumentsByacts({
-        ...docFilters,
-        actOpened: openedActs?.toString(),
-      }),
-    {
-      onSuccess({ successData }) {
-        setDocumentsByAct(successData);
-      },
-      enabled: !!(filters && token),
-      refetchOnWindowFocus: false,
-      keepPreviousData: true,
-    }
-  );
-
-  return (
-    <>
-      {isError && (
-        <div className="p-2">
-          <Alert classModifier="error" title="Une erreur est survenue lors du chargement des Actes." />
-        </div>
-      )}
-      <Loader mode={isFetching ? "get" : "none"} text="Chargement en cours">
-        {documentsByAct && (
-          <>
-            <EdiSubcribe subscribeEdi={subscribeEdi} subscriptionUrl={ediSubscriptionUrl} />
-            <ListActs />
-          </>
-        )}
-      </Loader>
-    </>
-  );
-};
-
-export default ActsApp;
-
+  La deuxieme : lorsque on  en ajouter  une nouvelle template aspose ilya un case a cocher qui dit :  Récupérer les documents versionnés est ce que cest une information a envoyer coté backend  et est ceq que on va fgarder dans le nouveau consolepfel 
