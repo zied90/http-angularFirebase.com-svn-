52f787e35653cdbf22f691bf75f777100839965b
zied <zied.elmiladi@gmail.com>
2025-05-15 14:32:42 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,1 @@
-2 point qui  ne sont pas claire dans  upload d’un document :
- La premiere :
-Ca veut dire quoi : Pour récupérer le MODOP "ASPOSE Outil Upload" cliquez sur village.axa-fr.intraxa 
-Et ce Modop consitre a faire  quoi  et est ce que ce lien est consomable ou non dans l ancienne version et est ce que on va le garder dans la nouvel console 
 
-  La deuxieme : lorsque on  en ajouter  une nouvelle template aspose ilya un case a cocher qui dit :  Récupérer les documents versionnés est ce que cest une information a envoyer coté backend  et est ceq que on va fgarder dans le nouveau consolepfel 
