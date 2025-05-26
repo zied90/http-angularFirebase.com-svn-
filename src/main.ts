5f70a3ed8672a74a34920108bd05a8c21827a32c
zied <zied.elmiladi@gmail.com>
2025-05-26 15:02:40 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,21 @@
-Hibernate: 
-    select
-        top (?) l1_0.id,
-        l1_0.action,
-        l1_0.client_request,
-        l1_0.time,
-        l1_0.name_user,
-        la1_0.app_name,
-        l1_0.num_contract,
-        l1_0.portfolio,
-        l1_0.server,
-        l1_0.is_error,
-        lt1_0.template,
-        l1_0.date_created 
-    from
-        t_logs l1_0 
-    join
-        t_logs_application la1_0 
-            on la1_0.id=l1_0.logs_application_id 
-    join
-        t_logs_template lt1_0 
-            on lt1_0.id=l1_0.logs_template_id 
-    order by
-        l1_0.date_created desc
-Hibernate: 
-    select
-        count_big(l1_0.id) 
-    from
-        t_logs l1_0
+package fr.axa.pfel.console;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import org.springframework.data.repository.query.Param;
+import java.util.Optional;
+
+@Repository
+public interface ILogRepository extends
+    PagingAndSortingRepository<Log, Long>,JpaRepository<Log, Long>, JpaSpecificationExecutor<Log>
+{
+
 
 
 	@Query("""
@@ -50,152 +39,33 @@ Hibernate:
 
 
 
+}
+
 package fr.axa.pfel.console;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
-@Builder
-@Getter @Setter
-@NoArgsConstructor
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 @AllArgsConstructor
-@Table(name = "T_LOGS")
-public class Log {
-
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ID")
+public class LogDTO {
     private Long id;
-
-    @Column(name = "ACTION", nullable = false)
     private String action;
-
-    @Column(name = "NUM_CLAIM")
-    private String numClaim;
-
-    @Column(name = "CLIENT_NAME")
-    private String clientName;
-
-    @Column(name = "NUM_CLIENT")
-    private String numClient;
-
-    @Column(name = "NUM_CONTRACT")
+    private String clientRequest;
+    private Double time;
+    private String userName;
+    private String appName;
     private String numContract;
-
-    @Column(name = "STATE_DOC")
-    private String stateDoc;
-
-    @Column(name = "ERROR")
-    private String error;
-
-    @Column(name = "DATE_CREATED")
+    private String portfolio;
+    private String server;
+    private boolean isError;
+    private String template;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime dateCreated;
-
-    @Column(name = "REQUEST")
-    private String request;
-
-    @JsonProperty("isEmail")
-    @Column(name = "IS_EMAIL", nullable = false)
-    private boolean isEmail;
-
-    @JsonProperty("isFax")
-    @Column(name = "IS_FAX", nullable = false)
-    private boolean isFax;
-
-    @JsonProperty("isGed")
-    @Column(name = "IS_GED", nullable = false)
-    private boolean isGed;
-
-    @JsonProperty("isLive")
-    @Column(name = "IS_LIVE", nullable = false)
-    private boolean isLive;
-
-    @JsonProperty("isMassPrinting")
-    @Column(name = "IS_MASS_PRINTING", nullable = false)
-    private boolean isMassPrinting;
-
-    @JsonProperty("isStored")
-    @Column(name = "IS_STORED", nullable = false)
-    private boolean isStored;
-
-    @JsonProperty("isWeb")
-    @Column(name = "IS_WEB", nullable = false)
-    private boolean isWeb;
-
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "LOGS_APPLICATION_ID")
-    private LogApplication logApplication;
-
-
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "LOGS_TEMPLATE_ID")
-    private LogTemplate logTemplate;
-
-    @Column(name = "NUM_DOC", nullable = false)
-    private Long numDoc;
-
-    @Column(name = "NUM_PAGES", nullable = false)
-    private Long numPages;
-
-    @Column(name = "PORTFOLIO")
-    private String portfolio;
-
-    @Column(name = "TIME", nullable = false)
-    private Double time;
-
-    @Column(name = "NAME_USER")
-    private String userName;
-
-    @Column(name = "SERVER")
-    private String server;
-
-    @Column(name = "TIME_GENERATION")
-    private Double timeGeneration;
-
-    @Column(name = "CLIENT_REQUEST")
-    private String clientRequest;
-
-    @Column(name = "INFORMATION")
-    private String information;
-
-    @Column(name = "NUM_SUBSCRIBER")
-    private String numSubscriber;
-
-    @JsonProperty("isError")
-    @Column(name = "IS_ERROR", nullable = false)
-    private boolean isError;
-
-    @Column(name = "SIZE_REQUEST")
-    private Long sizeRequest;
-
-    @Column(name = "SORTIE")
-    private String sortie;
-
-    @JsonProperty("isLocalPrinting")
-    @Column(name = "IS_LOCAL_PRINTING", nullable = false, columnDefinition = "BIT DEFAULT 0")
-    private boolean isLocalPrinting;
-
-
-    public Boolean isError() {
-        return Boolean.TRUE.equals(isError);
-    }
 
 }
