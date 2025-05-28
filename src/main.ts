ba5d6c845636bf715de84f6cd8f4b9d372d3186d
zied <zied.elmiladi@gmail.com>
2025-05-28 10:32:47 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,49 +1,46 @@
- private getHeadersAndRows(data: string[][]): { headers: string[]; rows: string[][] } {
-    let [headers, ...rows] = data;
-    console.log(headers, "headersheaders");
-    // detect if headers is an headers row, if we have an email in the headers, we can assume that the headers are not correct
-    if (/\w+@\w+.\w/.test(headers.join(" "))) {
-      const newHeadersRow = headers.map(() => "");
-      const actualHeaders = headers.map((h) => h);
-      // first find the header with the email
-      const emailHeaderIndex = actualHeaders.findIndex((header) => /\w+@\w+.\w/.test(header));
-      if (emailHeaderIndex >= 0) {
-        newHeadersRow[emailHeaderIndex] = "email";
-        actualHeaders[emailHeaderIndex] = "";
-      }
-      // find the header with the id
-      const idHeaderIndex = actualHeaders.findIndex((header) => /^\w\d{5,6}$/.test(header));
-      if (idHeaderIndex > -1) {
-        newHeadersRow[idHeaderIndex] = "id";
-        actualHeaders[idHeaderIndex] = "";
-      }
-      // find the header with the type
-      console.log(
-        actualHeaders.findIndex((header) => /admin|agt/i.test(header)),
-        "dddddddddd"
-      );
-      const typeHeaderIndex = actualHeaders.findIndex((header) => /admin|agt/i.test(header));
-      if (typeHeaderIndex > -1) {
-        newHeadersRow[typeHeaderIndex] = "type";
-        actualHeaders[typeHeaderIndex] = "";
-      }
-      // find the header with the lastname
-      const lastnameHeaderIndex = actualHeaders.findIndex((header) => /\w+/i.test(header));
-      if (lastnameHeaderIndex > -1) {
-        newHeadersRow[lastnameHeaderIndex] = "lastname";
-        actualHeaders[lastnameHeaderIndex] = "";
-      }
-      // find the header with the firstname
-      const firstnameHeaderIndex = actualHeaders.findIndex((header) => header !== "");
-      if (firstnameHeaderIndex >= 0) {
-        newHeadersRow[firstnameHeaderIndex] = "firstname";
-        actualHeaders[firstnameHeaderIndex] = "";
-      }
-
-      return { headers: newHeadersRow, rows: data };
-    } else {
-      return { headers, rows };
-    }
-  }
+import { FC, useMemo, useEffect, useState } from "react";
+import FormItem from "@/toolkit/Components/Form/FormItem";
+import { useDelayApi } from "@/hooks/useApi";
+import { allTypesRoute } from "@/Api/ApiRoutes";
 
 
+interface Type {
+  code: string;
+  name: string;
+}
+interface Props {
+  className?: string;
+}
+const Types: FC<Props> = ({ className = "" }) => {
+  const { call: loadTypes } = useDelayApi(allTypesRoute); 
+  const [types, setTypes] = useState<string[]>([]);
+  useEffect(() => {
+    (async () => {
+      try {
+        const response: string[] = await loadTypes();
+        setTypes(response);
+      } catch (error) {
+        console.error("Erreur lors du chargement des types :", error);
+      }
+    })();
+  }, [loadTypes]);
+  const typeDatas = useMemo(
+    () => [{ label: "Sélectionnez un type", value: "" }, ...types.map((e) => (e:=>e))],
+    [types]
+  );
+  return types.length ? (
+    <FormItem
+      id="id-Type-template"
+      labelStyle={className}
+      type="select"
+      label="ECM-Type"
+      name="type"
+      placeholder="Sélectionner un Type"
+      required={true}
+      visibleValue={types.length === 1 ? types[0].name : undefined}
+      value={types.length === 1 ? types[0].code : undefined}
+      datas={typeDatas}
+    />
+  ) : null;
+};
+export default Types;
