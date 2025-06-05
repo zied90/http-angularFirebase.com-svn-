ea940fee87611d55ee838184d44fe047eb0e5340
zied <zied.elmiladi@gmail.com>
2025-06-05 23:16:23 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
-   <div class="form-group">
-       <label class="col-xs-2 control-label" for="region">@WebResources.Perimetre </label>
-       <div class="col-xs-10">
-           <div class="radio">
-               <div class="radio-check-group" ng-repeat="perimetre in allPerimeters">
-                   <input id="{{$id}}" type="radio" name="Perimeter" value="{{perimetre.id}}" data-ng-model="contexte.perimetreId" ng-disabled="disablePerimeter(perimetre) || !editeContexteDemande" />
-                   <label for="{{$id}}" class="radio">{{ perimetre.libelle }}</label>
-               </div>
-           </div>
-       </div>
-   </div>
+    <div class="form-group">
+        <label class="col-xs-2 control-label">@WebResources.TypeDemandLabel</label>
+        <div class="col-xs-10">
+            <select class="form-control" ng-options="option.id as option.libelle for option in contextes.typeDemandes" ng-model="contexte.typeDemandeId" ng-disabled="!editeContexteDemande">
+                <option value="">@WebResources.Select</option>
+            </select>
+        </div>
+    </div>
