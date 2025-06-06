abfdb4cd25144e25fca29c9f359d7fbff219b759
zied <zied.elmiladi@gmail.com>
2025-06-06 16:03:05 +0000

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,3 @@
-  <div class="form-group radiocheck-custom" id="cg_{{element.dynHtmlId()}}">
-      <label class="col-xs-2 control-label" for="{{element.dynHtmlId()}}">{{ element.libelle }}</label>
-      <div class="col-xs-10" id='add_{{element.dynUniqueId()}}'>
-          <div class="radio-inline" ng-repeat='opt in element.valuesList'>
-              <input type="radio"
-                     ng-disabled='!element.myState.enabled'
-                     name="{{element.dynUniqueId()}}_{{opt.Identifiant}}"
-                     id="{{element.dynUniqueId()}}_{{opt.Identifiant}}"
-                     ng-model="element.getModel()[id]"
-                     ng-value="opt" />
-              <label for="{{element.dynUniqueId()}}_{{opt.Identifiant}}">{{ opt.Libelle }}</label>
-
-          </div>
-      </div>
-  </div>
+   <div class="form-group" id="isUploadNatifDiv">
+       <label class="control-label col-xs-5"><input type="checkbox" ng-model='isUploadNatifChosen' ng-disabled="isUploadNatifDisabled()" />@WebResources.AttachmentsNatifUpload </label>
+   </div>
