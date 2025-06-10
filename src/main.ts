949888b2293130d704b798cf2ca4ae1a5e4184fb
zied <zied.elmiladi@gmail.com>
2025-06-10 15:29:13 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,23 @@
-   <div class="form-group" id="isUploadNatifDiv">
-       <label class="control-label col-xs-5"><input type="checkbox" ng-model='isUploadNatifChosen' ng-disabled="isUploadNatifDisabled()" />@WebResources.AttachmentsNatifUpload </label>
-   </div>
+package fr.axa.pfel.console;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@Table(name = "T_LOGS_TEMPLATE")
+public class LogTemplate {
+    @Id
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "LOGS_APPLICATION_ID", referencedColumnName = "ID", nullable = false)
+    private LogApplication logApplication;
+
+    @Column(name = "TEMPLATE")
+    private String template;
+
+}
+ jutilise import org.springframework.data.jpa.repository.JpaRepository;
+ 
