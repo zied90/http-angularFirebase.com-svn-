d00ecffcf07ca2ed73c45e06e71c0e9e8a57bebb
zied <zied.elmiladi@gmail.com>
2025-06-11 15:10:08 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -36,7 +36,7 @@ export const SearchForm = React.forwardRef<
     portfolio: initialData?.portfolio || "",
     numContract: initialData?.numContract || "",
     app: initialData?.app || { applicationName: "", id: 0 },
-    template: initialData?.template || null, // stocker uniquement l'id du template ici
+    templateId: initialData?.templateId || null,
     isError: initialData?.isError || false,
     startDate: initialData?.startDate ? new Date(initialData.startDate) : null,
     endDate: initialData?.endDate ? new Date(initialData.endDate) : null,
@@ -96,6 +96,7 @@ export const SearchForm = React.forwardRef<
     onData(formData as any);
     onSubmit(e);
   };
+  console.log(initialData, "initialData");
   // Gestion changement de select application
   const handleSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
     const selectedApp = applications.find(
@@ -110,7 +111,7 @@ export const SearchForm = React.forwardRef<
   };
   // Pour afficher dans le Typeahead l'option sélectionnée à partir de l'id
   const selectedTemplateOption = templateOptions.filter(
-    (opt) => opt.id === formData.template
+    (opt) => opt.id === formData.templateId
   );
   return (
     <form onSubmit={handleSubmit} ref={ref}>
@@ -232,17 +233,17 @@ export const SearchForm = React.forwardRef<
               onInputChange={(text) => {
                 setQueryTemplate(text);
                 if (text.length < 2) {
-                  setFormData({ ...formData, template: 0 });
+                  setFormData({ ...formData, templateId: 0 });
                 }
               }}
               onChange={(selected) => {
                 if (selected.length > 0) {
                   setFormData({
                     ...formData,
-                    template: (selected[0] as TemplateOption).id,
+                    templateId: (selected[0] as TemplateOption).id,
                   });
                 } else {
-                  setFormData({ ...formData, template: 0 });
+                  setFormData({ ...formData, templateId: 0 });
                 }
               }}
               clearButton
@@ -265,269 +266,3 @@ export const SearchForm = React.forwardRef<
     </form>
   );
 });
-
-
-import { useQuery, useQueryClient } from "react-query";
-import { useState, useMemo, useRef } from "react";
-import Loader from "@axa-fr/react-toolkit-loader";
-import { Alert } from "@axa-fr/react-toolkit-all";
-import { LogsTable } from "./logsTable";
-import logStore from "@/stores/logs/logsStore";
-import {
-  findLogs,
-  getLogs,
-  massReplay,
-  replay,
-} from "@/api/apifunctions/logs/logs";
-import Pagination from "@/components/Pagination";
-import usePagination from "@/hooks/pagination/usePagination";
-import { Button } from "@/components/atoms/index";
-import CustomModal from "@/components/modals/customModal";
-import { defColumnsLogs } from "@/constants/columns";
-import { ILog, searchParams } from "@/types/logs";
-import useSort from "@/components/SortButton/useSort";
-
-import ExpandedLogRow from "@/pages/logs/logsTable/ExpandedLogRow";
-import { Replay } from "./Replay";
-import useGenericMutation from "@/hooks/mutation/useGenericMutation";
-import { renderActions } from "@/components/renderActions";
-import { useLogicModal } from "@/hooks/logicModal/useLogicModal";
-import { ConfirmationModal } from "@/components/modals/ConfirmationModal";
-import { SearchForm } from "./Search";
-export const LogsList = () => {
-  const sort = useSort();
-  const { setPagination, getSearchParams } = usePagination();
-  const { numberItems, page } = getSearchParams();
-  const [selectedRowIds, setSelectedRowIds] = useState<number[]>([]);
-  const [logID, setLogID] = useState<number | null>(null);
-  const [searchParams, setSearchParams] = useState<searchParams | null>(null);
-  const formRef = useRef<HTMLFormElement>(null);
-  const { sortBy, sortDirection } = sort;
-  const [errors, setErrors] = useState<string[]>([]);
-  const { logs, setLogs } = logStore();
-  const [replayData, setReplayData] = useState<string>("");
-  const { openModal, closeModal, isModalOpen, modalMode } =
-    useLogicModal<ILog>();
-  const logsFilters = useMemo(
-    () => ({
-      page: page - 1,
-      size: numberItems,
-      order: sortDirection,
-      sortBy: sortBy || "id",
-    }),
-    [page, numberItems, sortDirection, sortBy]
-  );
-  const queryClient = useQueryClient();
-  const handleError = (message: string) => {
-    setErrors([message]);
-  };
-  const { isFetching } = useQuery(
-    ["logs", logsFilters],
-    () => getLogs(logsFilters),
-    {
-      onSuccess: ({ successData }) => {
-        setLogs(successData);
-      },
-      onError: () => {
-        handleError(
-          "Une erreur s'est produite lors de la récupération de la liste des logs"
-        );
-      },
-      enabled: !searchParams,
-      refetchOnWindowFocus: false,
-      refetchOnMount: false,
-    }
-  );
-  console.log("debug", searchParams, logsFilters);
-  const dataSearch = useQuery(
-    ["logs_search", searchParams, logsFilters],
-    () => findLogs(searchParams, logsFilters),
-    {
-      onSuccess: ({ successData }) => {
-        setLogs(successData);
-        closeModal();
-      },
-      onError: () => {
-        handleError("Une erreur s'est produite lors de la recherche des logs");
-      },
-      enabled: !!searchParams,
-      refetchOnWindowFocus: false,
-    }
-  );
-  const handleRenderAction = (log: ILog) => {
-    return log.action === "GENERATE"
-      ? renderActions({
-          showReplay: true,
-          onReplay: () => {
-            setLogID(log.id);
-            openModal("replay", log);
-          },
-        })
-      : null;
-  };
-
-  const { mutate: massReplayMutate, isLoading: loadingMassReplay } =
-    useGenericMutation(
-      massReplay,
-      "Rejeu en masse réussi !",
-      "Erreur lors du rejeu en masse !",
-      () => {
-        // Rafraîchir la liste des logs après le replay
-        if (searchParams) {
-          queryClient.invalidateQueries("logs_search");
-        } else {
-          queryClient.invalidateQueries("logs");
-        }
-        setSelectedRowIds([]);
-        closeModal();
-      }
-    );
-  const { mutate: replayMutate, isLoading: loadingReplay } = useGenericMutation(
-    replay,
-    "Rejouer avec succès !",
-    "Erreur lors du rejeu !",
-    () => {
-      setLogs({
-        totalPages: 0,
-        size: 0,
-        totalElements: 0,
-        content: [],
-        currentPage: 0,
-        isLast: false,
-      });
-      if (searchParams) {
-        queryClient.invalidateQueries("logs_search");
-      } else {
-        queryClient.invalidateQueries("logs");
-      }
-
-      closeModal();
-    }
-  );
-  const handleSubmitReplay = () => {
-    replayMutate(replayData);
-  };
-
-  const handleDataSearch = (data: searchParams) => {
-    handlePageChange(1);
-    setSearchParams(data);
-    closeModal();
-  };
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-  };
-  const handlePageChange = (newPage: number) =>
-    setPagination({ page: newPage });
-
-  const handleNumPerPageChange = (numPerPage: number) =>
-    setPagination({ numberItems: numPerPage, page: 1 });
-
-  const handleSelectedRows = (rows: number[]) => {
-    setSelectedRowIds(rows);
-  };
-  const handleMassReplay = () => {
-    if (selectedRowIds.length > 0) {
-      openModal("massReplay");
-    }
-  };
-  return (
-    <div className="mt-3">
-      {errors.length ? (
-        errors.map((error: string, index: number) => (
-          <Alert key={index} classModifier="error" title={error}></Alert>
-        ))
-      ) : (
-        <div>
-          <div className="row g-2 mb-2">
-            <div className="col-auto ">
-              <Button onClick={() => openModal("rechercher")}>
-                <span className="af-btn__text">Recherche</span>
-              </Button>
-            </div>
-            <div className="col-auto">
-              {selectedRowIds.length > 0 && (
-                <Button onClick={handleMassReplay} disabled={loadingMassReplay}>
-                  <span className="af-btn__text">Replay</span>
-                </Button>
-              )}
-            </div>
-          </div>
-          <Loader
-            mode={isFetching || dataSearch.isFetching ? "get" : "none"}
-            className="af-loader"
-            text=""
-          >
-            {!isFetching ? (
-              logs?.content && logs?.content.length > 0 ? (
-                <LogsTable
-                  columns={defColumnsLogs}
-                  items={logs?.content || []}
-                  renderExpandedRow={(item) => <ExpandedLogRow item={item} />}
-                  renderActions={(item) => handleRenderAction(item)}
-                  onSelectedRows={handleSelectedRows}
-                />
-              ) : (
-                <Alert classModifier="info" title="Aucune log(s) trouvée." />
-              )
-            ) : null}
-          </Loader>
-          {logs?.totalPages > 1 && (
-            <div className="pagination-wrapper mb-1">
-              <Pagination
-                page={page}
-                pagesCount={logs?.totalPages}
-                numPerPage={numberItems}
-                onPageChange={handlePageChange}
-                onNumberItemsChange={handleNumPerPageChange}
-              />
-            </div>
-          )}
-        </div>
-      )}
-      <CustomModal
-        title={"Recherche"}
-        isOpen={isModalOpen && modalMode === "rechercher"}
-        onClose={closeModal}
-        onCancel={() => {
-          setSearchParams(null);
-          closeModal();
-        }}
-        cancelTitle="Rénitialiser"
-        onSubmit={() => formRef.current?.requestSubmit()}
-        submitTitle={"Rechercher"}
-        dataTestid="modal-search"
-        isForm={true}
-      >
-        <SearchForm
-          ref={formRef}
-          onSubmit={handleSubmit}
-          onData={handleDataSearch}
-          initialData={searchParams}
-        />
-      </CustomModal>
-      <Replay
-        id={logID}
-        isOpen={isModalOpen && modalMode === "replay"}
-        onClose={closeModal}
-        onChangeValueReplay={setReplayData}
-        isLoading={loadingReplay}
-        onSubmit={handleSubmitReplay}
-      />
-
-      <ConfirmationModal
-        item={selectedRowIds}
-        isOpen={isModalOpen && modalMode === "massReplay"}
-        onClose={closeModal}
-        actionFn={() => massReplayMutate(selectedRowIds)}
-        itemIdExtractor={(item) => item[0]}
-        itemNameExtractor={() => "logs sélectionnés"}
-        message="Êtes-vous sûr de vouloir rejouer les logs sélectionnés ?"
-        submitTitle="Oui, Rejouer"
-        cancelTitle="Annuler"
-        isError={false}
-        isLoading={loadingMassReplay}
-      />
-    </div>
-  );
-};
- 
