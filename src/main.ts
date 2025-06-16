e4b1c73e8b3d6f035fb6c64075d40682f3f80bf3
zied <zied.elmiladi@gmail.com>
2025-06-16 10:48:25 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,2 @@
 
+<label class="control-label col-xs-5"><input type="checkbox" ng-model="isUploadNatifChosen" ng-disabled="isUploadNatifDisabled()" class="ng-valid ng-dirty">Cochez cette case pour charger les documents Word au format initial</label>
