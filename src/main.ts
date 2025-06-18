a3e9bea0759e114c69043689683f0ff0bdfdb071
zied <zied.elmiladi@gmail.com>
2025-06-18 12:36:35 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,201 +1,60 @@
 package fr.axa.pfel.console;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import java.time.LocalDateTime;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@Entity
-@Builder
-@Getter @Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Table(name = "T_LOGS")
-public class Log {
+import java.util.List;
 
+public interface ITemplateRepository extends JpaRepository<LogTemplate, Long> {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ID")
-    private Long id;
+    List<LogTemplate> findByTemplate(String template);
+}
 
-    @Column(name = "ACTION", nullable = false)
-    private String action;
+package fr.axa.pfel.console.logs.impl;
 
-    @Column(name = "NUM_CLAIM")
-    private String numClaim;
+import fr.axa.pfel.console.ITemplateRepository;
+import fr.axa.pfel.console.LogTemplate;
+import fr.axa.pfel.console.logs.ITemplateService;
+import org.springframework.stereotype.Service;
 
-    @Column(name = "CLIENT_NAME")
-    private String clientName;
+import java.util.List;
 
-    @Column(name = "NUM_CLIENT")
-    private String numClient;
+@Service
+public class TemplateService implements ITemplateService {
 
-    @Column(name = "NUM_CONTRACT")
-    private String numContract;
+    private final ITemplateRepository templateRepository;
 
-    @Column(name = "STATE_DOC")
-    private String stateDoc;
-
-    @Column(name = "ERROR")
-    private String error;
-
-    @Column(name = "DATE_CREATED")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
-    private LocalDateTime dateCreated;
-
-    @Column(name = "REQUEST")
-    private String request;
-
-    @JsonProperty("isEmail")
-    @Column(name = "IS_EMAIL", nullable = false)
-    private boolean isEmail;
-
-    @JsonProperty("isFax")
-    @Column(name = "IS_FAX", nullable = false)
-    private boolean isFax;
-
-    @JsonProperty("isGed")
-    @Column(name = "IS_GED", nullable = false)
-    private boolean isGed;
-
-    @JsonProperty("isLive")
-    @Column(name = "IS_LIVE", nullable = false)
-    private boolean isLive;
-
-    @JsonProperty("isMassPrinting")
-    @Column(name = "IS_MASS_PRINTING", nullable = false)
-    private boolean isMassPrinting;
-
-    @JsonProperty("isStored")
-    @Column(name = "IS_STORED", nullable = false)
-    private boolean isStored;
-
-    @JsonProperty("isWeb")
-    @Column(name = "IS_WEB", nullable = false)
-    private boolean isWeb;
-
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "LOGS_APPLICATION_ID")
-    private LogApplication logApplication;
-
-
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "LOGS_TEMPLATE_ID")
-    private LogTemplate logTemplate;
-
-    @Column(name = "NUM_DOC", nullable = false)
-    private Long numDoc;
-
-    @Column(name = "NUM_PAGES", nullable = false)
-    private Long numPages;
-
-    @Column(name = "PORTFOLIO")
-    private String portfolio;
-
-    @Column(name = "TIME", nullable = false)
-    private Double time;
-
-    @Column(name = "NAME_USER")
-    private String userName;
-
-    @Column(name = "SERVER")
-    private String server;
-
-    @Column(name = "TIME_GENERATION")
-    private Double timeGeneration;
-
-    @Column(name = "CLIENT_REQUEST")
-    private String clientRequest;
-
-    @Column(name = "INFORMATION")
-    private String information;
-
-    @Column(name = "NUM_SUBSCRIBER")
-    private String numSubscriber;
-
-    @JsonProperty("isError")
-    @Column(name = "IS_ERROR", nullable = false)
-    private boolean isError;
-
-    @Column(name = "SIZE_REQUEST")
-    private Long sizeRequest;
-
-    @Column(name = "SORTIE")
-    private String sortie;
-
-    @JsonProperty("isLocalPrinting")
-    @Column(name = "IS_LOCAL_PRINTING", nullable = false, columnDefinition = "BIT DEFAULT 0")
-    private boolean isLocalPrinting;
-
-
-    public Boolean isError() {
-        return Boolean.TRUE.equals(isError);
+    public TemplateService(ITemplateRepository templateRepository) {
+        this.templateRepository = templateRepository;
     }
 
+    @Override
+    public List<LogTemplate> searchLogTemplates(String term) {
+
+        return templateRepository.findByTemplate(term);
+    }
 }
 
 package fr.axa.pfel.console;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
-import org.springframework.data.repository.query.Param;
-import java.util.Optional;
+@Entity
+@Getter @Setter
+@Table(name = "T_LOGS_TEMPLATE")
+public class LogTemplate {
+    @Id
+    private Long id;
 
-@Repository
-public interface ILogRepository extends
-    PagingAndSortingRepository<Log, Long>,JpaRepository<Log, Long>, JpaSpecificationExecutor<Log>
-{
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "LOGS_APPLICATION_ID", referencedColumnName = "ID", nullable = false)
+    private LogApplication logApplication;
 
-	@Query("SELECT new Action(l.action) FROM Log l GROUP BY l.action")
-	Iterable<Action> findAllDistinctActions();
-
-	@Query("""
-  SELECT new fr.axa.pfel.console.LogDTO(
-      l.id,
-      l.action,
-      l.clientRequest,
-      l.time,
-      l.userName,
-      l.logApplication.appName,
-      l.numContract,
-      l.portfolio,
-      l.server,
-      l.isError,
-      l.logTemplate.template,
-      l.dateCreated
-  )
-  FROM Log l
-""")
-
-	Page<LogDTO> findAllSummarized(Pageable pageable);
-
-	@Query("SELECT new fr.axa.pfel.console.LogDetailDTO(" +
-			"l.id, l.request, l.information, l.numClient, l.numSubscriber, l.numClaim, " +
-			"l.clientName, l.time, l.numPages, l.numDoc, l.isWeb, l.isMassPrinting, " +
-			"l.isGed, l.isEmail, l.isStored, l.isLive, l.isLocalPrinting, l.error) " +
-			"FROM Log l WHERE l.id = :id")
-	Optional<LogDetailDTO> findDetailById(@Param("id") Long id);
-
+    @Column(name = "TEMPLATE")
+    private String template;
 
 }
-fait   @EntityGraph 
+
+
+
