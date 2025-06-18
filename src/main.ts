2e83aea8a376af624ece5e911ab0922b6283a391
zied <zied.elmiladi@gmail.com>
2025-06-18 15:03:39 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,90 +1,210 @@
 package fr.axa.pfel.console;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
 
-import java.util.List;
+import java.time.LocalDateTime;
 
-public interface ITemplateRepository extends JpaRepository<LogTemplate, Long> {
-
-    List<LogTemplate> findByTemplateContainingIgnoreCase(String template);
-}
-package fr.axa.pfel.console;
-
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-
-@Entity
-@Getter @Setter
-@Table(name = "T_LOGS_TEMPLATE")
-@NamedEntityGraph(name="template",attributeNodes = {@NamedAttributeNode("id"),@NamedAttributeNode("template")})
-public class LogTemplate {
-    @Id
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LogDTO {
     private Long id;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "LOGS_APPLICATION_ID", referencedColumnName = "ID", nullable = false)
-    private LogApplication logApplication;
-
-    @Column(name = "TEMPLATE")
+    private String action;
+    private String clientRequest;
+    private Double time;
+    private String userName;
+    private String appName;
+    private String numContract;
+    private String portfolio;
+    private String server;
+    private boolean isError;
     private String template;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime dateCreated;
 
 }
 
-
-@Service
-public class TemplateService implements ITemplateService {
-
-    private final ITemplateRepository templateRepository;
-
-    public TemplateService(ITemplateRepository templateRepository) {
-        this.templateRepository = templateRepository;
-    }
-
-    @Override
-    public List<TemplateDTO> searchLogTemplates(String template) {
-        List<TemplateDTO> templates=new ArrayList<>();
-
-        var result= templateRepository.findByTemplateContainingIgnoreCase(template);
-        for (var r:result){
-          templates.add(TemplateDTO.builder().id(r.getId()).template(r.getTemplate()).build())  ;
-        }
-        return templates;
-    }
-}
 
 package fr.axa.pfel.console;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
-public class TemplateDTO {
+public class LogDetailDTO {
     private Long id;
-    private String template;
+    private String request;
+    private String information;
+    private String numClient;
+    private String numSubscriber;
+    private String numClaim;
+    private String clientName;
+    private Double time;
+    private Long numPages;
+    private Long numDoc;
+    private boolean isWeb;
+    private boolean isMassPrinting;
+    private boolean isGed;
+    private boolean isEmail;
+    private boolean isStored;
+    private boolean isLive;
+    private boolean isLocalPrinting;
+    private String error;
 }
 
 
-package fr.axa.pfel.console.mapper;
 
-import fr.axa.pfel.console.Application;
-import fr.axa.pfel.consolepfel.spoolnet.client.model.ApplicationUpdate;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
+package fr.axa.pfel.console;
 
-@Mapper
-public interface ApplicationMapper {
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-    ApplicationMapper INSTANCE = Mappers.getMapper(ApplicationMapper.class);
+@Entity
+@Builder
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "T_LOGS")
+public class Log {
 
-    @Mapping(source = "name", target = "nameApp")
-    @Mapping(source = "spool", target = "isSpool")
-    @Mapping(target = "password", defaultValue = "N/A")
-    Application toEntity(ApplicationUpdate dto);
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "ACTION", nullable = false)
+    private String action;
+
+    @Column(name = "NUM_CLAIM")
+    private String numClaim;
+
+    @Column(name = "CLIENT_NAME")
+    private String clientName;
+
+    @Column(name = "NUM_CLIENT")
+    private String numClient;
+
+    @Column(name = "NUM_CONTRACT")
+    private String numContract;
+
+    @Column(name = "STATE_DOC")
+    private String stateDoc;
+
+    @Column(name = "ERROR")
+    private String error;
+
+    @Column(name = "DATE_CREATED")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime dateCreated;
+
+    @Column(name = "REQUEST")
+    private String request;
+
+    @JsonProperty("isEmail")
+    @Column(name = "IS_EMAIL", nullable = false)
+    private boolean isEmail;
+
+    @JsonProperty("isFax")
+    @Column(name = "IS_FAX", nullable = false)
+    private boolean isFax;
+
+    @JsonProperty("isGed")
+    @Column(name = "IS_GED", nullable = false)
+    private boolean isGed;
+
+    @JsonProperty("isLive")
+    @Column(name = "IS_LIVE", nullable = false)
+    private boolean isLive;
+
+    @JsonProperty("isMassPrinting")
+    @Column(name = "IS_MASS_PRINTING", nullable = false)
+    private boolean isMassPrinting;
+
+    @JsonProperty("isStored")
+    @Column(name = "IS_STORED", nullable = false)
+    private boolean isStored;
+
+    @JsonProperty("isWeb")
+    @Column(name = "IS_WEB", nullable = false)
+    private boolean isWeb;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "LOGS_APPLICATION_ID")
+    private LogApplication logApplication;
+
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "LOGS_TEMPLATE_ID")
+    private LogTemplate logTemplate;
+
+    @Column(name = "NUM_DOC", nullable = false)
+    private Long numDoc;
+
+    @Column(name = "NUM_PAGES", nullable = false)
+    private Long numPages;
+
+    @Column(name = "PORTFOLIO")
+    private String portfolio;
+
+    @Column(name = "TIME", nullable = false)
+    private Double time;
+
+    @Column(name = "NAME_USER")
+    private String userName;
+
+    @Column(name = "SERVER")
+    private String server;
+
+    @Column(name = "TIME_GENERATION")
+    private Double timeGeneration;
+
+    @Column(name = "CLIENT_REQUEST")
+    private String clientRequest;
+
+    @Column(name = "INFORMATION")
+    private String information;
+
+    @Column(name = "NUM_SUBSCRIBER")
+    private String numSubscriber;
+
+    @JsonProperty("isError")
+    @Column(name = "IS_ERROR", nullable = false)
+    private boolean isError;
+
+    @Column(name = "SIZE_REQUEST")
+    private Long sizeRequest;
+
+    @Column(name = "SORTIE")
+    private String sortie;
+
+    @JsonProperty("isLocalPrinting")
+    @Column(name = "IS_LOCAL_PRINTING", nullable = false, columnDefinition = "BIT DEFAULT 0")
+    private boolean isLocalPrinting;
+
+
+    public Boolean isError() {
+        return Boolean.TRUE.equals(isError);
+    }
+
 }
