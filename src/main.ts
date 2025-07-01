4addc28d9062d9e76ef2fd70d8c33737544fcfcf
zied <zied.elmiladi@gmail.com>
2025-07-01 09:10:34 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,1 @@
-const formatEsignUrl = (config: IConfiguration, params: any) => {
-  // remove null values
-  Object.keys(params).forEach((key) => (params[key] === null || params[key] === undefined ? delete params[key] : null));
-  return getDomainForUrl(config.urls.esign, config) + new URLSearchParams(params).toString().replace(/%3B/g, ";");
-};
+
