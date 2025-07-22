91d6d7a7fcfa6e7f692bb81df75eea97b14959f5
zied <zied.elmiladi@gmail.com>
2025-07-22 12:10:55 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,55 @@
-const decryptWithKey = (text: string, key: string): string => {
-  let decryptedText = "";
-  for (let i = 0; i < text.length; i++) {
-    const charCode = text.charCodeAt(i) - key.charCodeAt(i % key.length);
-    decryptedText += String.fromCharCode(charCode);
-  }
-  return decryptedText;
+  if (!allDocsHaveSameClientNumber(docs)) {
+      const result = await showModalNotSameNumber({ documents: docs });
+      if (result === "close") {
+        resolverCancel();
+      }
+      return;
+    }
+
+
+import CustomModal from "@/components/modals/customModal";
+import { IDocument } from "@/types/documents";
+import { create, InstanceProps } from "react-modal-promise";
+
+export const showModalNotSameNumber = async ({ documents }: { documents: IDocument[] }) => {
+  const modal: React.FC<InstanceProps<unknown>> = ({ isOpen, onResolve, onReject }) => {
+    const close = () => {
+      onResolve("close");
+    };
+
+    return (
+      <CustomModal
+        title={"Message"}
+        isOpen={isOpen}
+        onSubmit={close}
+        onClose={close}
+        submitTitle="OK"
+        dataTestid="modal-not-same-numbers"
+      >
+        Vous avez sélectionné des documents qui n'ont pas les mêmes identifiants (Numéro de contrat). Voici la liste des
+        identifiants :
+        <pre>
+          <code>
+            <strong>
+              {documents.map((document: IDocument, index: number) => (index ? ", " : "") + document.contractNumber)}
+            </strong>
+          </code>
+        </pre>
+      </CustomModal>
+    );
+  };
+  const promiseModal = create(modal);
+  return promiseModal({ isOpen: true });
 };
 
-const KEY = "Pl0L_uB@jNJ5%dByQv7Ho&UO{TBF_k[zhw1y#0)e@q?4i]Ty4ns;hEfe$3CnS{o";
+export const allDocsHaveSameClientNumber = (docs: IDocument[]) => {
+  const clientNumber = removeLeadingZeros(docs[0].clientNumber + "");
+  return docs.every((doc) => removeLeadingZeros(doc.clientNumber + "") === clientNumber);
+};
 
-uKmkvtTaaKunwryqioqvtsXorK2VnpLD7cmn
+
+Règle d'autorisation d'envoi par mail :
+ * Si on a des numéros de client différents => blocage
+ * Si on a des numéros clients identiques => envoi autorisé
+ * Si on a 1 numéro de client et d'autres documents sans numéro client => envoi autorisé avec le numéro client qu'on a
+ * Si on a aucun numéro client => envoi autorisé (champ du destinataire vide)
